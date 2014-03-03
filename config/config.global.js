6d96b9f1f2c304a1410767980cf0b8c7222e34b1
Sarin Kizhakkepurayil <sarink@juniper.net>
2014-03-03 14:51:26 -0800

    ceph REST Server config hook

--- config/config.global.js
+++ config/config.global.js
@@ -27,6 +27,12 @@ config.storageManager = {};
 config.storageManager.ip = '127.0.0.1';
 config.storageManager.port = '8776';
 
+//Ceph Rest API Server and port
+config.ceph={}
+config.ceph.enabled=true;
+config.ceph.rest_ip='127.0.0.1'
+config.ceph.rest_port='5002';
+
 // VNConfig API server and port.
 config.cnfg = {};
 config.cnfg.server_ip = '127.0.0.1';
