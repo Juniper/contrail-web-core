9905e74f4146a35501fa83c208e118faa8dbb435
surakula <surakula@juniper.net>
2014-04-10 12:43:19 -0700

    added new property for osd status to calculate num of out and down osds

--- src/serverroot/web/api/storage/ceph.infra.api.js
+++ src/serverroot/web/api/storage/ceph.infra.api.js
@@ -130,6 +130,16 @@ function getStorageOSDSummary(req, res, appData){
 
 function parseStorageOSDData(osdJSON){
     var osdMapJSON ={};    
+    var num_osds= jsonPath(osdJSON, "$.output.num_osds")[0];
+    var num_up_osds= jsonPath(osdJSON, "$.output.num_up_osds")[0];
+    var num_in_osds= jsonPath(osdJSON, "$.output.num_up_osds")[0];
+    
+    var num_down_osds = num_osds - num_up_osds;
+    var num_out_osds = num_osds -num_in_osds;
+    var obj = jsonPath(osdJSON, "$.output")[0];
+    obj['num_down_osds'] = num_down_osds;
+    obj['num_out_osds'] = num_out_osds;
+    c = obj;
     osdMapJSON['osd_stat']= osdJSON;
     return osdMapJSON;
 }
