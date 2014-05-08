f49fa0c299dcb9fe5ca2083287091cbf89f928c0
Biswajit Mandal <bmandal@juniper.net>
2014-05-08 21:38:29 +0530

    1. Removing the code to install node in code, node/npm is prerequisite for webUI    code, no need to install it if not available 2. In Gruntfile.js, corrected the path for qunit modules in dev-env

--- generate-files.sh
+++ generate-files.sh
@@ -4,23 +4,6 @@
 # Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
 #
 
-INSTALL_DIR=$PWD/../build/contrail-web-third-party
-PATH=$INSTALL_DIR/bin:$PATH
-if command -v node >/dev/null 2; then {
-    echo "Node already installed"
-} else {
-    mkdir -p $INSTALL_DIR
-
-    echo "Installing NodeJS ..."
-    cd ../contrail-web-third-party/node-v*
-    ./configure --prefix=$INSTALL_DIR
-    make
-    make install
-    echo "Installed NodeJS"
-    cd -
-}
-fi
-
 if command -v nodejs > /dev/null; then
   node_exec=nodejs;
 elif command -v node > /dev/null; then
