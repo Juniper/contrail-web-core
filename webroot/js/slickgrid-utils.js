fa90385c802fdb7bff5e1bcfcdae4e42c8e8b2d3
Vishnu Vardhan V <vishnuvv@juniper.net>
2014-11-21 15:39:10 +0530

    Related-Bug:#1371917 Problem is refresh view is getting called two times one in onDataUpdate and other is onUpdateData,onDataUpdate was added recently Fix is removed the grid refresh related code in the onDataUpdate event handler in the slickgrid,going forward need to merge the onUpdateData with onDataUpate
    Change-Id: I78c5e63bf76f3a65f63eb8356c7c616dd4ebd2f9

--- webroot/js/slickgrid-utils.js
+++ webroot/js/slickgrid-utils.js
@@ -733,18 +733,15 @@ function getDefaultGridConfig() {
                     }
                 }, 50);
             };
+            /**
+             * Going forward need to merge onUpdateData with onDataUpdate  
+             */
             eventHandlerMap.dataView['onDataUpdate'] = function(e,args) {
                 //Refresh the grid only if it's not destroyed
                 if($(gridContainer).data('contrailGrid')) {
-                    grid.invalidateAllRows();
-                    grid.updateRowCount();
-                    grid.render();
                     if(contrail.checkIfFunction(gridDataSource.events.onDataUpdate)) {
                         gridDataSource.events.onDataUpdate();
                     }
-                    if(gridDataSource.dataView != null && gridDataSource.dataView.getItems().length == 0)
-                        gridContainer.data('contrailGrid').showGridMessage('empty');
-                    gridContainer.data('contrailGrid').refreshView();
                 }
             };
             eventHandlerMap.dataView['onUpdateData'] = function () {
