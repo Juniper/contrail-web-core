ec89c68da13b177bab7c914b46e26063d63deb4e
Sagar Gala <sgala@juniper.net>
2015-02-06 11:32:06 -0800

    Added FormSelect2DropdownView.js

---
+++ webroot/js/views/FormSelect2DropdownView.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'backbone'
+], function (_, Backbone) {
+    var FormSelect2DropdownView = Backbone.View.extend({
+        render: function () {
+            var dropdownTemplate = contrail.getTemplate4Id(cowc.TMPL_SELECT2_DROPDOWN_VIEW),
+                viewConfig = this.attributes.viewConfig,
+                elId = this.attributes.elementId,
+                elementConfig = viewConfig[cowc.KEY_ELEMENT_CONFIG],
+                path = viewConfig[cowc.KEY_PATH],
+                lockEditingByDefault = this.attributes.lockEditingByDefault,
+                labelValue = (elId != null) ? smwl.get(elId) : smwl.get(path),
+                tmplParameters;
+
+            if (!(contrail.checkIfExist(lockEditingByDefault) && lockEditingByDefault)) {
+                lockEditingByDefault = false;
+            }
+            this.model.initLockAttr(path, lockEditingByDefault);
+
+            tmplParameters = {
+                label: labelValue, id: elId, name: elId,
+                dataBindValue: viewConfig[cowc.KEY_DATABIND_VALUE],
+                class: "span12",
+                lockAttr: lockEditingByDefault,
+                elementConfig: elementConfig
+            };
+
+            this.$el.html(dropdownTemplate(tmplParameters));
+
+        }
+    });
+
+    return FormSelect2DropdownView;
+});
