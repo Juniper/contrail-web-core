61a48cbfa6218c28f06d9ae99e510a39df6d89e2
Biswajit Mandal <bmandal@juniper.net>
2015-02-26 16:59:15 +0530

    Adding initial APIs to get the config delta. While doing a PUT, diff will be generated between whatever has come from UI client and whatever already configured in API Server, only those changed config will be sent to API Server. These diff will be calculated based on some interested fields, and some mandatory fields which will be sent irrespective of change or not (like fq_name, uuid etc fields.
    Related-Bug: #1423815
    
    Change-Id: Id49c8ce3c251bda4730b87813dbfd0fff44aec90

---
+++ src/serverroot/common/configDelta.utils.js
@@ -0,0 +1,55 @@
+var commonUtils = require('../utils/common.utils');
+
+var configJsonModifyObj = {
+    'virtual-network': {
+        'preProcessJSONDiff': modifyConfigData,
+        'optFields': ['virtual_network_properties',
+            'network_ipam_refs', 'network_policy_refs',
+            'route_target_list', 'floating_ip_pools'],
+        'mandateFields': ['fq_name', 'uuid']
+    }
+};
+
+var configArrSkipObjs = ['href', 'uuid'];
+
+function configArrAttrFound (configObj)
+{
+    for (key in configObj) {
+        if (key == 'attr') {
+            return true;
+        }
+    }
+    return false;
+}
+
+function modifyConfigData (type, configData, optFields, mandateFields)
+{
+    var newConfigData = commonUtils.cloneObj(configData[type]);
+    var optFieldsLen = 0;
+    if (null != optFields) {
+        optFieldsLen = optFields.length;
+    }
+    var configArrSkipObjsLen = configArrSkipObjs.length;
+    for (var i = 0; i < optFieldsLen; i++) {
+        if (newConfigData[optFields[i]] instanceof Array) {
+            var newConfigDataFieldsLen = newConfigData[optFields[i]].length;
+            for (var j = 0; j < newConfigDataFieldsLen; j++) {
+                if (false ==
+                        configArrAttrFound(newConfigData[optFields[i]][j])) {
+                    continue;
+                }
+                for (var k = 0; k < configArrSkipObjsLen; k++) {
+                    if (null != newConfigData[optFields[i]][j][configArrSkipObjs[k]]) {
+                        delete newConfigData[optFields[i]][j][configArrSkipObjs[k]];
+                    }
+                }
+            }
+        }
+    }
+    var resultJSON = {};
+    resultJSON[type] = newConfigData;
+    return resultJSON;
+}
+
+exports.configJsonModifyObj = configJsonModifyObj;
+
---
+++ src/serverroot/common/jsondiff.js
@@ -0,0 +1,140 @@
+var jsondiffpatch = require('jsondiffpatch');
+var configApiServer = require('./configServer.api');
+var commonUtils = require('../utils/common.utils');
+var configDeltaUtils = require('./configDelta.utils');
+var logutils = require('../utils/log.utils');
+
+var diffpatcher = jsondiffpatch.create({
+    objectHash: function(obj, index) {
+        // try to find an id property, otherwise just use the index in the array
+        return obj.name || obj.id || obj._id || obj._id || '$$index:' + index;
+    },
+    arrays: {
+        detectMove: true,
+        includeValueOnMove: true
+    }
+});
+
+function buildConfigDeltaJson (delta, oldJson, newJson, type, optFields,
+                               mandateFields)
+{
+    var resultJSON = {};
+
+    if (null != delta) {
+        delta = delta[type];
+    }
+    if (null != oldJson) {
+        oldJson = oldJson[type];
+    }
+    if (null != newJson) {
+        newJson = newJson[type];
+    }
+    var optFieldsCnt = optFields.length;
+    resultJSON[type] = {};
+    for (var i = 0; i < optFieldsCnt; i++) {
+        if ((null != delta[optFields[i]]) && (null != newJson) &&
+            (null != newJson[optFields[i]])) {
+            resultJSON[type][optFields[i]] = newJson[optFields[i]];
+        }
+    }
+    if (null == mandateFields) {
+        return resultJSON;
+    }
+    var mandateFieldsCnt = mandateFields.length;
+    for (var i = 0; i < mandateFieldsCnt; i++) {
+        if (null != newJson[mandateFields[i]]) {
+            resultJSON[type][mandateFields[i]] = newJson[mandateFields[i]];
+        } else if (null != oldJson[mandateFields[i]]) {
+            resultJSON[type][mandateFields[i]] = oldJson[mandateFields[i]];
+        }
+    }
+    return resultJSON;
+}
+
+function getConfigJSONDiff (type, oldJson, newJson)
+{
+    var optFields = [];
+    var mandateFields = [];
+
+    var fieldsObj = getConfigFieldsByType(type);
+    if (null != fieldsObj.error) {
+        logutils.logger.error("Config Diff: Not found the type: " + type);
+        return null;
+    }
+    optFields = fieldsObj['optFields'];
+    mandateFields = fieldsObj['mandateFields'];
+    var delta = diffpatcher.diff(oldJson, newJson);
+    if ((null == delta) || (undefined == delta) ||
+        ('undefined' == delta)) {
+        return null;
+    }
+    if ((null != configDeltaUtils.configJsonModifyObj[type]) &&
+        (null !=
+         configDeltaUtils.configJsonModifyObj[type]['preProcessJSONDiff'])) {
+        var jsonModCB =
+            configDeltaUtils.configJsonModifyObj[type]['preProcessJSONDiff'];
+        oldJson = jsonModCB(type, oldJson, optFields, mandateFields);
+    }
+
+    return buildConfigDeltaJson(delta, oldJson, newJson, type, optFields,
+                                mandateFields);
+}
+
+function getConfigFieldsByType (type)
+{
+    var error = null;
+    var optFields = [];
+    var mandateFields = [];
+    if (null == configDeltaUtils.configJsonModifyObj[type]) {
+        error = new appErrors.RESTServerError('type ' + type + ' not ' +
+                                              'specified in ' +
+                                              'configJsonModifyObj');
+        return {'error': error};
+    }
+    var configTypeObj = configDeltaUtils.configJsonModifyObj[type];
+    if (null == configTypeObj['optFields']) {
+        error = new appErrors.RESTServerError('fields not ' +
+                                              'specified in ' +
+                                              'configJsonModifyObj');
+        return {'error': error};
+    }
+    optFields = configTypeObj['optFields'];
+    if (null != configTypeObj['mandateFields']) {
+        mandateFields = configTypeObj['mandateFields'];
+    }
+    return {'error': error, 'optFields': optFields, 'mandateFields': mandateFields};
+}
+
+function getJSONDiffByConfigUrl (url, appData, newJson, callback)
+{
+    var error = null;
+    var optFields = [];
+    var mandateFields = [];
+    if (null == url) {
+        error = new appErrors.RESTServerError('URL not specified');
+        callback(error, null);
+        return;
+    }
+    var urlArrStr = url.split(/[/]/);
+    if ((null == urlArrStr) || (urlArrStr.length < 2)) {
+        error =
+            new appErrors.RESTServerError('URL ' + url + ' format not ' +
+                                          'correct.');
+        callback(error, null);
+        return;
+    }
+    var type = urlArrStr[1];
+
+    configApiServer.apiGet(url, appData, function(err, configData) {
+        if ((null != err) || (null == configData)) {
+            callback(err, configData);
+            return;
+        }
+        var delta = getConfigJSONDiff(type, configData, newJson);
+        callback(err, delta);
+    });
+}
+
+exports.getConfigJSONDiff = getConfigJSONDiff;
+exports.getJSONDiffByConfigUrl = getJSONDiffByConfigUrl;
+
