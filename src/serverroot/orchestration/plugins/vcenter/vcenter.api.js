04c9eec2909b2cf720c935c250cee2e56d110189
Naga Kiran <nagakiran@juniper.net>
2015-03-02 18:52:03 +0530

    Closes-Bug: #1427213
    Currently, Create Networks fails if there exists an ip-pool already with
    the required name.
    Handled by deleting the ip-pool if already exists and re-create the new
    ip-pool with the user-given parameters
    
    Change-Id: I340361f845d6344563015f71583d6984b0a78f05

--- src/serverroot/orchestration/plugins/vcenter/vcenter.api.js
+++ src/serverroot/orchestration/plugins/vcenter/vcenter.api.js
@@ -64,7 +64,17 @@ function queryIpPools(appData) {
                     }
                 }
             },appData,function(err,data,resHeaders) {
-                resolve(data);
+                var ipPoolsList = [];
+                if(data['QueryIpPoolsResponse'] instanceof Array)
+                    ipPoolsList = data['QueryIpPoolsResponse'][0]['_value']['returnval'];
+                else
+                    ipPoolsList = data['QueryIpPoolsResponse']['returnval'];
+                var ipPoolMap = {};
+                //Create a map with ip-pool name as key and id as value
+                for(var i=0;i<ipPoolsList.length;i++) {
+                    ipPoolMap[ipPoolsList[i]['name']] = ipPoolsList[i]['id'];
+                }
+                resolve(ipPoolMap);
             });
         });
     });
@@ -448,8 +458,23 @@ function createNetwork(userData,appData,callback) {
                 getIdByMobName(appData,'DistributedVirtualPortgroup',userData['name'],folderName).done(function(response) {
                     portGroupId = response;
                     userData['portGroupId'] = portGroupId;
-                    createIpPool(userData,appData,callback).done(function(response) {
-                        callback(null,response);
+                    //Check if ip-pool already exists
+                    queryIpPools(appData).done(function(response) {
+                        if(response['ip-pool-for-' + userData['name']] != null) {
+                            destroyIpPool(appData,response['ip-pool-for-' + userData['name']]).done(function(response) {
+                                if(response['Fault'] != null) {
+                                    callback(null,response);
+                                    return;
+                                }
+                                createIpPool(userData,appData,callback).done(function(response) {
+                                    callback(null,response);
+                                });
+                            });
+                        } else {
+                                createIpPool(userData,appData,callback).done(function(response) {
+                                    callback(null,response);
+                                });
+                        }
                     });
                 });
             });
