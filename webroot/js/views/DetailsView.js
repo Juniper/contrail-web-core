60eafaa51631cc2d6c12bc819b50962b9f2d6c22
Abhi <singla@juniper.net>
2015-03-04 09:15:28 -0800

    Don't load templates again if already loaded.

--- webroot/js/views/DetailsView.js
+++ webroot/js/views/DetailsView.js
@@ -8,13 +8,16 @@ define([
 ], function (_, Backbone) {
     var DetailsView = Backbone.View.extend({
         render: function () {
-            var viewConfig = this.attributes.viewConfig,
+            var loadingSpinnerTemplate = contrail.getTemplate4Id(cowc.TMPL_LOADING_SPINNER),
+                viewConfig = this.attributes.viewConfig,
                 ajaxConfig = viewConfig['ajaxConfig'],
                 templateConfig = viewConfig['templateConfig'],
                 dataParser = viewConfig['dataParser'],
                 app = viewConfig['app'],
                 detailsTemplate = cowu.generateDetailTemplate(templateConfig, app),
                 self = this;
+
+            self.$el.append(loadingSpinnerTemplate);
 
             contrail.ajaxHandler(ajaxConfig, null, function(response) {
                 self.$el.html(detailsTemplate(dataParser(response)));
