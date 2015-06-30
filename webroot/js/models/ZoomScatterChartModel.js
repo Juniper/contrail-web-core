8ff9de81af70d24001c4760f9e94824e24269e86
Sagar Gala <sgala@juniper.net>
2015-06-30 13:36:24 -0700

    Enhancements - Added filter for ZoomScatter Charts - Added allowing of Bootstrap modal to be dragged and save/close on keyboard Enter/Esc

--- webroot/js/models/ZoomScatterChartModel.js
+++ webroot/js/models/ZoomScatterChartModel.js
@@ -8,113 +8,117 @@ define([
     var ZoomScatterChartModel = function (dataListModel, chartConfig) {
         var self = this, forceX = chartConfig.forceX,
             forceY = chartConfig.forceY, margin = chartConfig.margin,
-            rawData = dataListModel.getFilteredItems(),
             chartData, d3Scale;
 
-        self.data = chartConfig['dataParser'](rawData);
-        self.margin = margin,
+        self.margin = margin;
         self.noDataMessage = chartConfig['noDataMessage'];
-
-        chartData = self.data;
-        self.sizeFieldName = contrail.handleIfNull(chartConfig['sizeFieldName'], 'size');
-
-        self.sizeMinMax = getSizeMinMax(chartData, self.sizeFieldName);
-
-        d3Scale = d3.scale.linear().range([6, 10]).domain(self.sizeMinMax);
-
-        $.each(chartData, function (idx, chartDataPoint) {
-            chartDataPoint['size'] = contrail.handleIfNaN(d3Scale(chartDataPoint[self.sizeFieldName]), 6);
-        });
-
-        self.width = chartConfig['width'] - margin.left - margin.right;
-        self.height = chartConfig['height'] - margin.top - margin.bottom;
-
-        self.xMin = 0;
-        self.yMin = 0;
-
-        if(chartData.length > 0) {
-            self.xMax = Math.ceil(d3.max(chartData, function (d) {
-                return +d[chartConfig.xField];
-            }) * 1.1);
-
-            self.yMax = Math.ceil(d3.max(chartData, function (d) {
-                return +d[chartConfig.yField];
-            }) * 1.1);
-
-            if (self.xMax <= 0) {
-                self.xMax = 1;
-            }
-
-            if (self.yMax <= 0) {
-                self.yMax = 1;
-            }
-
-        } else {
-            self.xMax = 0;
-            self.yMax = 0;
-        }
-
-        if (forceX) {
-            if (self.xMin > forceX[0]) {
-                self.xMin = forceX[0];
-            }
-
-            if (self.xMax < forceX[1]) {
-                self.xMax = forceX[1];
-            }
-        }
-
-        if (forceY) {
-            if (self.yMin > forceY[0]) {
-                self.yMin = forceY[0];
-            }
-
-            if (self.yMax < forceY[1]) {
-                self.yMax = forceY[1];
-            }
-        }
-
-        self.xScale = d3.scale.linear().domain([self.xMin, self.xMax]).range([0, self.width]);
-        self.yScale = d3.scale.linear().domain([self.yMin, self.yMax]).range([self.height, 0]);
-
-        self.zoomBehavior = d3.behavior.zoom().x(self.xScale).y(self.yScale).scaleExtent([1, 4]);
-
-        self.maxColorFilterFields = d3.max(chartData, function (d) {
-            return +d[chartConfig.colorFilterFields]
-        });
 
         self.classes = ['error', 'warning', 'medium', 'okay', 'default'];
 
-        self.xAxis = d3.svg.axis().scale(self.xScale).orient("bottom").ticks(10)
-            .tickSize(-self.height)
-            .tickFormat(contrail.checkIfFunction(chartConfig.xLabelFormat) ? chartConfig.xLabelFormat : d3.format("d"));
-        self.yAxis = d3.svg.axis().scale(self.yScale).orient("left").ticks(5)
-            .tickSize(-self.width)
-            .tickFormat(contrail.checkIfFunction(chartConfig.yLabelFormat) ? chartConfig.yLabelFormat : d3.format("d"))
+        self.refresh = function() {
+            var rawData = dataListModel.getFilteredItems();
+            self.data = contrail.checkIfFunction(chartConfig['dataParser']) ? chartConfig['dataParser'](rawData) : rawData;
 
-        self.xMed = median(_.map(chartData, function (d) {
-            return d[chartConfig.xField];
-        }));
+            chartData = self.data;
+            self.sizeFieldName = contrail.handleIfNull(chartConfig['sizeFieldName'], 'size');
+            self.sizeMinMax = getSizeMinMax(chartData, self.sizeFieldName);
 
-        self.yMed = median(_.map(chartData, function (d) {
-            return d[chartConfig.yField];
-        }));
+            d3Scale = d3.scale.linear().range([6, 10]).domain(self.sizeMinMax);
 
-        self.getNoDataMessage = function() {
-            updateNoDataMessage(dataListModel, self);
-            return self.noDataMessage;
+            $.each(chartData, function (idx, chartDataPoint) {
+                chartDataPoint['size'] = contrail.handleIfNaN(d3Scale(chartDataPoint[self.sizeFieldName]), 6);
+            });
+
+            self.width = chartConfig['width'] - margin.left - margin.right;
+            self.height = chartConfig['height'] - margin.top - margin.bottom;
+
+            self.xMin = 0;
+            self.yMin = 0;
+
+            if(chartData.length > 0) {
+                self.xMax = Math.ceil(d3.max(chartData, function (d) {
+                    return +d[chartConfig.xField];
+                }) * 1.1);
+
+                self.yMax = Math.ceil(d3.max(chartData, function (d) {
+                    return +d[chartConfig.yField];
+                }) * 1.1);
+
+                if (self.xMax <= 0) {
+                    self.xMax = 1;
+                }
+
+                if (self.yMax <= 0) {
+                    self.yMax = 1;
+                }
+
+            } else {
+                self.xMax = 0;
+                self.yMax = 0;
+            }
+
+            if (forceX) {
+                if (self.xMin > forceX[0]) {
+                    self.xMin = forceX[0];
+                }
+
+                if (self.xMax < forceX[1]) {
+                    self.xMax = forceX[1];
+                }
+            }
+
+            if (forceY) {
+                if (self.yMin > forceY[0]) {
+                    self.yMin = forceY[0];
+                }
+
+                if (self.yMax < forceY[1]) {
+                    self.yMax = forceY[1];
+                }
+            }
+
+            self.xScale = d3.scale.linear().domain([self.xMin, self.xMax]).range([0, self.width]);
+            self.yScale = d3.scale.linear().domain([self.yMin, self.yMax]).range([self.height, 0]);
+
+            self.zoomBehavior = d3.behavior.zoom().x(self.xScale).y(self.yScale).scaleExtent([1, 4]);
+
+            self.maxColorFilterFields = d3.max(chartData, function (d) {
+                return +d[chartConfig.colorFilterFields]
+            });
+
+            self.xAxis = d3.svg.axis().scale(self.xScale).orient("bottom").ticks(10)
+                .tickSize(-self.height)
+                .tickFormat(contrail.checkIfFunction(chartConfig.xLabelFormat) ? chartConfig.xLabelFormat : d3.format("d"));
+            self.yAxis = d3.svg.axis().scale(self.yScale).orient("left").ticks(5)
+                .tickSize(-self.width)
+                .tickFormat(contrail.checkIfFunction(chartConfig.yLabelFormat) ? chartConfig.yLabelFormat : d3.format("d"))
+
+            self.xMed = median(_.map(chartData, function (d) {
+                return d[chartConfig.xField];
+            }));
+
+            self.yMed = median(_.map(chartData, function (d) {
+                return d[chartConfig.yField];
+            }));
+
+            self.getNoDataMessage = function() {
+                updateNoDataMessage(dataListModel, self, chartConfig);
+                return self.noDataMessage;
+            };
         };
+
+        self.refresh();
 
         return self;
     };
 
-    function updateNoDataMessage (dataListModel, chartModel) {
+    function updateNoDataMessage (dataListModel, chartModel, chartConfig) {
         var error = dataListModel.error;
 
         if (dataListModel.isRequestInProgress()) {
             chartModel.noDataMessage = cowc.CHART_LOADING_MESSAGE;
-        } else if (contrail.checkIfExist(chartModel['noDataMessage'])) {
-            //Nothing to do
+        } else if (contrail.checkIfExist(chartConfig['noDataMessage'])) {
+            chartModel.noDataMessage = chartConfig.noDataMessage;
         } else if (error) {
             chartModel.noDataMessage = cowc.DATA_ERROR_MESSAGE;
         } else if(dataListModel.getItems().length == 0) {
