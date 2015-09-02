d973575e4b3b279eaa471168c8980e605c9a5990
sagargala <gala.sagar.s@gmail.com>
2015-09-02 14:15:54 -0700

    Fixed custom time range related logic and styles for FormButtonView under QE MVC

--- webroot/js/views/FormButtonView.js
+++ webroot/js/views/FormButtonView.js
@@ -8,20 +8,22 @@ define([
 ], function (_, ContrailView) {
     var FormInputView = ContrailView.extend({
         render: function () {
-            var buttonTemplate = contrail.getTemplate4Id(cowc.TMPL_BUTTON_VIEW),
-                viewConfig = this.attributes.viewConfig,
-                label = this.attributes.label,
-                elId = this.attributes.elementId,
-                app = this.attributes.app,
-                visible = this.attributes.visible,
+            var self = this,
+                buttonTemplate = contrail.getTemplate4Id(cowc.TMPL_BUTTON_VIEW),
+                viewConfig = self.attributes.viewConfig,
+                label = self.attributes.label,
+                elId = self.attributes.elementId,
+                app = self.attributes.app,
+                visible = self.attributes.visible,
+                elementConfig = viewConfig.elementConfig,
                 labelValue = (label != null)? label :((elId != null) ? cowl.get(elId, app) : cowl.get(path, app)),
                 tmplParameters;
 
             tmplParameters = {
-                label: labelValue, id: elId, name: elId, visible: visible, class: "span3"
+                label: labelValue, id: elId, name: elId, visible: visible, class: "span3", elementConfig: elementConfig
             };
 
-            this.$el.html(buttonTemplate(tmplParameters));
+            self.$el.html(buttonTemplate(tmplParameters));
         }
     });
 
