dbeefc550fbe2f1586625b460cce855fd21a2709
Manoj Naik <mnaik@juniper.net>
2015-09-15 11:57:59 +0530

    Related-Bug: #1488346 - Added LoginWindowView and LoginWindowModel for showing the login window for contrail-status in Mon Infra Node Details pages.
    Change-Id: I5edd1365510787b488961aa823939322f8adfd0a

---
+++ webroot/js/models/LoginWindowModel.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-model'
+], function (_, ContrailModel) {
+    var LoginWindowModel = ContrailModel.extend({
+        defaultConfig: {
+            'user_name': "",
+            'password': ''
+        },
+        validations: {
+            loginValidations: {
+                'user_name': {
+                    required: true,
+                    msg: 'Enter User Name'
+                },
+                'password': {
+                    required: true,
+                }
+            }
+        },
+        doLogin: function (data, callbackObj) {
+            var ajaxConfig = {}, returnFlag = false;
+
+            if (this.model().isValid(true, "llsConfigValidations")) {
+                var locks = this.model().attributes.locks.attributes;
+                var loginData = this.model().attributes;
+                var putData = {"username": loginData.user_name,"password":loginData.password};
+                ajaxConfig = {};
+                ajaxConfig.async = false;
+                ajaxConfig.type = "POST";
+                ajaxConfig.data = JSON.stringify(putData);
+                ajaxConfig.dataType = "text";
+                ajaxConfig.url = '/api/service/networking/device-status/' + 
+                                        data['ip'];
+                contrail.ajaxHandler(ajaxConfig, function () {
+                    if (contrail.checkIfFunction(callbackObj.init)) {
+                        callbackObj.init();
+                    }
+                }, function (response) {
+                     console.log(response);
+                     if (contrail.checkIfFunction(callbackObj.success)) {
+                         callbackObj.success(response);
+                     }
+                     returnFlag = true;
+                }, function (error) {
+                     console.log(error);
+                     if (contrail.checkIfFunction(callbackObj.error)) {
+                         callbackObj.error(error);
+                     }
+                     returnFlag = false;
+                });
+            } else {
+                if (contrail.checkIfFunction(callbackObj.error)) {
+                    callbackObj.error(this.getFormErrorText(ctwl.
+                            LINK_LOCAL_SERVICES_PREFIX_ID));
+                }
+            }
+            return returnFlag;
+        },
+    });
+    return LoginWindowModel;
+});
+
