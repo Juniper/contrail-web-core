b3d668a5987b343c297a64ece3c9716220e459c5
Naga Kiran <nagakiran@juniper.net>
2015-10-07 17:34:32 +0530

    Added hardRefresh in Monitor > Infra > Dashboard
    Change-Id: Ib3ab50cade3ce1f5073be6115da1e5307ac65b11
    
    Added Handlebars helper function "truncate" to truncate the text to
    certain no of characters and show ellipsis notation
    
    Change-Id: I650cf26c632f0c7aa711586964f8cde6dd4b891f

--- webroot/js/handlebars-utils.js
+++ webroot/js/handlebars-utils.js
@@ -299,3 +299,14 @@ Handlebars.registerHelper('printJSON', function(jsonObject) {
     return JSON.stringify(jsonObject);
 });
 
+Handlebars.registerHelper ('truncate', function (str, len) {
+    if (str.length > len && str.length > 0) {
+        var new_str = str + " ";
+        new_str = str.substr (0, len);
+        new_str = str.substr (0, new_str.lastIndexOf(" "));
+        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+        return new Handlebars.SafeString ( new_str +'...' ); 
+    }
+    return str;
+});
