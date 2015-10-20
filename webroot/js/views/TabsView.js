3a4173d9741c373a7e074923dc9ae10a228693ff
sagargala <gala.sagar.s@gmail.com>
2015-10-19 17:45:30 -0700

    Implemented renderOnActivate for tabs

--- webroot/js/views/TabsView.js
+++ webroot/js/views/TabsView.js
@@ -13,24 +13,62 @@ define([
                 tabsTemplate = contrail.getTemplate4Id(cowc.TMPL_TABS_VIEW),
                 viewConfig = self.attributes.viewConfig,
                 tabs = viewConfig['tabs'],
-                validation = self.attributes.validation,
-                lockEditingByDefault = self.attributes.lockEditingByDefault,
-                modelMap = self.modelMap,
-                childViewObj, childElId, childView;
+                tabsIdMap = {}, tabRendered = [];
 
             self.$el.html(tabsTemplate({elementId: elId, tabs: tabs}));
 
-            var tabsData = $('#' + elId).contrailTabs({
-                active: contrail.handleIfNull(viewConfig.active, 0),
-                activate: viewConfig.activate,
-                theme: viewConfig.theme
-            }).data('contrailTabs');
+            $.each(tabs, function(tabKey, tabValue) {
+                tabsIdMap[tabValue[cowc.KEY_ELEMENT_ID] + '-tab'] = tabKey;
+                if (contrail.checkIfKeyExistInObject(true, 'tabValue.tabConfig', 'renderOnActivate') &&  tabValue.tabConfig.renderOnActivate === true) {
+                    tabRendered.push(false);
+                } else {
+                    self.renderTab(tabValue);
+                    tabRendered.push(true);
+                }
+            });
 
-            for (var i = 0; i < tabs.length; i++) {
-                childViewObj = tabs[i];
-                childElId = childViewObj[cowc.KEY_ELEMENT_ID];
-                childView = self.renderView4Config(this.$el.find("#" + childElId), this.model, childViewObj, validation, lockEditingByDefault, modelMap);
-            }
+            $('#' + elId).contrailTabs({
+                active: contrail.handleIfNull(viewConfig.active, 0),
+                activate: function( event, ui ) {
+                    var tabId = ($(ui.newPanel[0]).attr('id')),
+                        tabKey = tabsIdMap[tabId];
+
+                    if (contrail.checkIfFunction(viewConfig.activate)) {
+                        viewConfig.activate(event, ui);
+                    } else if (contrail.checkIfExist(tabs[tabKey].tabConfig) && contrail.checkIfFunction(tabs[tabKey].tabConfig.activate)) {
+                        tabs[tabKey].tabConfig.activate(event, ui);
+                    }
+                },
+                beforeActivate: function( event, ui ) {
+                    var tabId = ($(ui.newPanel[0]).attr('id')),
+                        tabKey = tabsIdMap[tabId];
+
+                    if (tabRendered[tabKey] === false) {
+                        self.renderTab(tabs[tabKey]);
+                        tabRendered[tabKey] = true;
+                    }
+                },
+                create: function( event, ui ) {
+                    var tabId = ($(ui.panel[0]).attr('id')),
+                        tabKey = tabsIdMap[tabId];
+
+                    if (tabRendered[tabKey] === false) {
+                        self.renderTab(tabs[tabKey]);
+                        tabRendered[tabKey] = true;
+                    }
+                },
+                theme: viewConfig.theme
+            });
+        },
+
+        renderTab: function(tabObj) {
+            var self = this,
+                validation = self.attributes.validation,
+                lockEditingByDefault = self.attributes.lockEditingByDefault,
+                modelMap = self.modelMap,
+                childElId = tabObj[cowc.KEY_ELEMENT_ID];
+
+            self.renderView4Config(this.$el.find("#" + childElId), this.model, tabObj, validation, lockEditingByDefault, modelMap);
         }
     });
 
