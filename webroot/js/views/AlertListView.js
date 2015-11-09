e0ff6378c4a2102a5a2d47368778c017e39f4ec9
Naga Kiran <nagakiran@juniper.net>
2015-11-09 21:26:19 +0530

    Related-Bug: #1495456 Moved Monitor Infra Dashboard functionality to core repository from controller repo such that feature repositories can add new info boxes in Dashboard
    Change-Id: I140d4162d8159abf3e8965448d6928826dab314a

---
+++ webroot/js/views/AlertListView.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'backbone',
+    'contrail-view',
+],function(_,Backbone,ContrailView) {
+    var AlertListView = ContrailView.extend({
+        initialize: function(options) {
+        },
+        renderAlerts: function() {
+            var self = this;
+            var alertListTmpl = contrail.getTemplate4Id('alerts-template');
+            var alertList = self.model.getItems();
+
+            self.$el.find('.widget-body .widget-main').
+                html(alertListTmpl(alertList.slice(0,5)));
+            self.$el.find('.widget-header').initWidgetHeader({
+                title: 'Alerts'
+            });
+
+            self.$el.find('#moreAlertsLink').click(function() {
+                cowu.loadAlertsPopup({
+                    model: self.model
+                });
+            });
+        },
+        render: function() {
+            var self = this;
+            self.renderAlerts();
+            self.model.onDataUpdate.subscribe(function() {
+                self.renderAlerts();
+            });
+        }
+    });
+    return AlertListView;
+});
