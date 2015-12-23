f3c537dc39e5a6aa85fbf5a04be93d33edeeacb8
balamurugang <gbala@juniper.net>
2015-12-23 13:58:14 +0530

    Routing Policy.
    * Formating is done for Routing Policy.
    * Alternative color is specified for term rows in Routing policy.
    * For TextArea added the support for specifying the row count from configration.
    * Added the footer as parameter for createModal
      so that the button can be editable.
    
    Change-Id: Iad59494700aa3cb4f2a55b5a0e3ba444520bd177
    Related-Bug: #1528776

--- webroot/js/common/core.utils.js
+++ webroot/js/common/core.utils.js
@@ -66,7 +66,7 @@ define(['underscore'], function (_) {
         this.createModal = function (options) {
             var modalId = options['modalId'],
                 footer = [];
-
+            if(options['footer'] == null || options['footer'] == undefined) {
             if ((contrail.checkIfExist(options['onClose'])) && (contrail.checkIfFunction(options['onClose']))) {
                 footer.push({
                     id        : 'closeBtn',
@@ -98,7 +98,9 @@ define(['underscore'], function (_) {
                     onKeyupEnter: true
                 });
             }
-
+            } else {
+                footer = options['footer'];
+            }
             $.contrailBootstrapModal({
                 id: modalId,
                 className: options['className'],
