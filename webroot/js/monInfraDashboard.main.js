fbde5843316ed1588022ea31158776fd69ded448
Naga Kiran <nagakiran@juniper.net>
2015-12-30 15:35:17 +0530

    Related-Bug: #1435268
    ScatterChart changes:
    Moved Bucketization changes to ZoomScatterChart
    1. Added Color filter for scatterchart
    2. Drill-down by drawing a region on scatterChart using mouse drag and
        show the select x/y filters (CPU: 0.3 - 0.8) above the chart
    3. Made bucketization as a config option (doBucketize) to scatterChart
    4. Display nodes with non X/Y values at intersection of X/Y axis
    
    In vRouter Summary page:
    1. Keeping all the 3 components (ScatterChart,Grid,crossFilter) in sync
    
    1. Added a wrapper for crossfilter (cf.datasource.js) that helps in
        keeping track of the current filters on crossFilter and triggering
        the events when ever new filter added/removed.
    
    1. Moved manageCrossFitlers to cfDataSource
    (webroot/js/common/cf.datasource.js)
    2. Moved d3.custom.barChart to barChart in BarChartView.js
    
    Change-Id: If546396d0d840788ad86f81f56f158164e056917

--- webroot/js/monInfraDashboard.main.js
+++ webroot/js/monInfraDashboard.main.js
@@ -9,21 +9,14 @@ function MonInfraDashboardLoader() {
         var self = this, currMenuObj = globalObj.currMenuObj,
             hashParams = paramObject['hashParams'],
             rootDir = currMenuObj['resources']['resource'][0]['rootDir'],
-            // pathControllerDashboardView = rootDir + '/js/views/ControllerDashboardView.js',
-            // pathMonitorInfraDashboardView = rootDir + '/js/views/MonitorInfraDashboardView.js',
             renderFn = paramObject['function'];
 
         if (self.monInfraDashboardView == null) {
-            // requirejs([pathControllerDashboardView,pathMonitorInfraDashboardView], function (ControllerDashboardView,MonitorInfraDashboardView) {
             requirejs(['mon-infra-dashboard-view'], function (MonitorInfraDashboardView) {
                 var monitorInfraDashboardView = MonitorInfraDashboardView;
                 monitorInfraDashboardView.render({
                     el:$(contentContainer)
                 });
-                // self.monInfraDashboardView = new ControllerDashboardView({
-                //     el: $(contentContainer)
-                // });
-                // self.monInfraDashboardView.render();
             });
         } else {
             self.renderView(renderFn, hashParams);
