6276220e753ea0be24ea782260e6237e95ce0761
Manoj Naik <mnaik@juniper.net>
2016-01-08 17:59:02 +0530

    Related-Bug: #1444824 - Moved the alarms.api.js to core. Reorganized some code to move common code to core and controller related code to controller.
    Change-Id: I0488a05149dd55f253db2baa4bd048c398f6fc27

---
+++ webroot/js/views/AlarmView.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view'
+], function (_, ContrailView) {
+    var MonitorAlarmsView = ContrailView.extend({
+        el: $(contentContainer),
+        renderAlarms: function (viewConfig) {
+            this.renderView4Config(this.$el, null, getAlarmsConfig());
+        }
+    });
+
+    function getAlarmsConfig() {
+        return {
+            elementId: cowu.formatElementId([ctwl.MONITOR_ALARMS_PAGE_ID]),
+            view: "AlarmListView",
+            viewPathPrefix: "js/views/",
+            app: cowc.APP_CONTRAIL_CONTROLLER,
+            viewConfig: {}
+        }
+    };
+    return MonitorAlarmsView;
+});
