b77d6b69fabfa8ee76ca08c199240812b8239940
Biswajit Mandal <bmandal@juniper.net>
2016-02-12 04:38:52 +0530

    Related-Bug: #1532666 As recommendation is to have admin role only, so if the last project is of member role, then do not allow the user to login. If after login, some user is found to be member, then as API Server returns permission denied, so user will not be able to do anything, so user has to logout, and then the available admin role project will be set as cookie.
    Change-Id: I33c7370c4918f4bf1238bdb8c81ac3ddf8d4790b

--- src/serverroot/web/api/configServer.main.api.js
+++ src/serverroot/web/api/configServer.main.api.js
@@ -41,45 +41,95 @@ function getDefProjectByAppData (appData)
     return defProject;
 }
 
-function getAuthTokenByProject (req, defTokenObj, project)
+function getAuthTokenByProject (req, defTokenObj, project, callback)
 {
     if ((null != req.session.tokenObjs[project]) &&
         (null != req.session.tokenObjs[project]['token']) &&
         (null != req.session.tokenObjs[project]['token']['id'])) {
-        return {'project': project,
-            'token': req.session.tokenObjs[project]['token']['id']};
+        callback(null, {'project': project,
+                 'token': req.session.tokenObjs[project]['token']['id']});
+        return;
     }
     var defProject =
         commonUtils.getValueByJsonPath(defTokenObj,
                                        'tenant;name',
                                        null);
-    return {'project': defProject, 'token': defTokenObj['id']};
+    var defTokenId =
+        commonUtils.getValueByJsonPath(defTokenObj, 'id', null);
+    var plugins = require('./../../orchestration/plugins/plugins.api');
+    var adminProjList = plugins.getAdminProjectList(req);
+    if ((null == adminProjList) ||(!adminProjList.length)) {
+        callback(null, {'project': defProject, 'token': defTokenId});
+        return;
+    }
+    var tokenObjs = req.session.tokenObjs;
+    var tokenId = null;
+    for (key in tokenObjs) {
+        if (-1 != adminProjList.indexOf(key)) {
+            tokenId =
+                commonUtils.getValueByJsonPath(tokenObjs[key],
+                                               'token;id', null);
+            break;
+        }
+    }
+    if (null == tokenId) {
+        tokenId =
+            commonUtils.getValueByJsonPath(tokenObjs[key],
+                                           'token;id', null);
+        if (null == tokenId) {
+            callback(null, {'project': defProject, 'token': defTokenId});
+            return;
+        }
+    }
+
+    var userObj = {'tokenid': tokenId, 'tenant': project, 'req': req};
+    authApi.getUIUserRoleByTenant(userObj, function(err, roles, data) {
+        if (null == data) {
+            callback(null, {'project': defProject, 'token': defTokenId});
+            return;
+        }
+        var tokenObj = data['tokenObj'];
+        if ((null != err) || (null == tokenObj) ||
+            (null == tokenObj['token']) || (null == tokenObj['token']['id']) ||
+            (null == tokenObj['token']['tenant'])) {
+            callback(null, {'project': defProject, 'token': defTokenId});
+            return;
+        }
+        callback(null, {'project': tokenObj['token']['tenant']['name'],
+                 'token': tokenObj['token']['id']});
+    });
 }
 
-function configAppHeaders (headers, appData)
+function configAppHeaders (headers, appData, callback)
 {
     var defProject = getDefProjectByAppData(appData);
     var multiTenancyEnabled = commonUtils.isMultiTenancyEnabled();
     var xAuthTokenObj = null;
-    try {
-        var xAuthTokenObj =
-            getAuthTokenByProject(appData['authObj'].req,
-                                  appData['authObj']['defTokenObj'],
-                                  defProject);
+    if ((null == appData) || (null == appData['authObj'].req) ||
+        (null == appData['authObj']['defTokenObj'])) {
+        headers['X-Auth-Token'] = null;
+        headers['X_API_ROLE'] = null;
+        callback(headers);
+        return;
+    }
+    getAuthTokenByProject(appData['authObj'].req,
+                          appData['authObj']['defTokenObj'],
+                          defProject, function(err, xAuthTokenObj) {
         headers['X-Auth-Token'] = xAuthTokenObj['token'];
         if (true == multiTenancyEnabled) {
-            if (null != xAuthTokenObj['project']) {
-                headers['X_API_ROLE'] =
-                    appData['authObj'].req.session.userRoles[xAuthTokenObj['project']].join(',');
-            } else {
+            try {
+                if (null != xAuthTokenObj['project']) {
+                    headers['X_API_ROLE'] =
+                        appData['authObj'].req.session.userRoles[xAuthTokenObj['project']].join(',');
+                } else {
+                    headers['X_API_ROLE'] = null;
+                }
+            } catch(e) {
                 headers['X_API_ROLE'] = null;
             }
         }
-    } catch(e) {
-        headers['X-Auth-Token'] = null;
-        headers['X_API_ROLE'] = null;
-    }
-    return headers;
+        callback(headers);
+    });
 }
 
 function apiGet (reqUrl, appData, callback, appHeaders, stopRetry)
@@ -89,7 +139,7 @@ function apiGet (reqUrl, appData, callback, appHeaders, stopRetry)
     var multiTenancyEnabled = commonUtils.isMultiTenancyEnabled();
 
     var defProject = getDefProjectByAppData(appData);
-    headers = configAppHeaders(headers, appData);
+    configAppHeaders(headers, appData, function(headers) {
     headers = getHeaders(headers, appHeaders);
     configServer.api.get(reqUrl, function(err, data) {
         if (err) {
@@ -105,6 +155,7 @@ function apiGet (reqUrl, appData, callback, appHeaders, stopRetry)
             callback(null, data);
         }
     }, headers);
+  });
 }
 
 function apiPut (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
@@ -114,7 +165,7 @@ function apiPut (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
     var multiTenancyEnabled = commonUtils.isMultiTenancyEnabled();
 
     var defProject = getDefProjectByAppData(appData);
-    headers = configAppHeaders(headers, appData);
+    configAppHeaders(headers, appData, function(headers) {
     headers = getHeaders(headers, appHeaders);
 
     configServer.api.put(reqUrl, reqData, function(err, data) {
@@ -131,6 +182,7 @@ function apiPut (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
             callback(null, data);
         }
     }, headers);
+  });
 }
 
 function apiPost (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
@@ -140,7 +192,7 @@ function apiPost (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
     var multiTenancyEnabled = commonUtils.isMultiTenancyEnabled();
 
     var defProject = getDefProjectByAppData(appData);
-    headers = configAppHeaders(headers, appData);
+    configAppHeaders(headers, appData, function(headers) {
     headers = getHeaders(headers, appHeaders);
 
     configServer.api.post(reqUrl, reqData, function(err, data) {
@@ -157,6 +209,7 @@ function apiPost (reqUrl, reqData, appData, callback, appHeaders, stopRetry)
             callback(null, data);
         }
     }, headers);
+  });
 }
 
 function apiDelete (reqUrl, appData, callback, appHeaders, stopRetry)
@@ -166,7 +219,7 @@ function apiDelete (reqUrl, appData, callback, appHeaders, stopRetry)
     var multiTenancyEnabled = commonUtils.isMultiTenancyEnabled();
 
     var defProject = getDefProjectByAppData(appData);
-    headers = configAppHeaders(headers, appData);
+    configAppHeaders(headers, appData, function(headers) {
     headers = getHeaders(headers, appHeaders);
 
     configServer.api.delete(reqUrl, function(err, data) {
@@ -183,6 +236,7 @@ function apiDelete (reqUrl, appData, callback, appHeaders, stopRetry)
             callback(null, data);
         }
     }, headers);
+  });
 }
 
 exports.apiGet = apiGet;
