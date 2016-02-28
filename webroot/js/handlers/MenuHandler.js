d0b9a886d6f3e07aa92fd1e4d462fe0998b8685b
Naga Kiran <nagakiran@juniper.net>
2016-02-28 12:23:09 +0530

    Closes-Bug: #1549690 Fixed the issue that disabledFeatures in config file are shown up in dev-env. Updated the test mock data as we merged all the calls that fetch web-server config information into one call. Removed the ajax URLs from test cases that are merged in to web-server-info call.
    Change-Id: Ia35a9de2ee42a6017377de775a726eb189f22ec8

--- webroot/js/handlers/MenuHandler.js
+++ webroot/js/handlers/MenuHandler.js
@@ -34,15 +34,15 @@ define(['underscore'], function (_) {
                 }
             }
             $.get('/' + mFileName+ '?built_at=' + built_at, function (xml) {
-                        menuObj = $.xml2json(xml);
-                var disabledFeatures = globalObj['webServerInfo']['disabledFeatures'];
+                menuObj = $.xml2json(xml);
+                var disabledFeatures = {disabled: globalObj['webServerInfo']['disabledFeatures']};
                 var featurePkgsInfo = globalObj['webServerInfo']['featurePkgsInfo'];
-                        processXMLJSON(menuObj, disabledFeatures);
-                        var menuShortcuts = contrail.getTemplate4Id('menu-shortcuts')(menuHandler.filterMenuItems(menuObj['items']['item'], 'menushortcut', featurePkgsInfo));
-                        $("#sidebar-shortcuts").html(menuShortcuts);
-                        ['items']['item'] = menuHandler.filterMenuItems(menuObj['items']['item']);
-                        initMenuDefObj.resolve();
-                    });
+                processXMLJSON(menuObj, disabledFeatures);
+                var menuShortcuts = contrail.getTemplate4Id('menu-shortcuts')(menuHandler.filterMenuItems(menuObj['items']['item'], 'menushortcut', featurePkgsInfo));
+                $("#sidebar-shortcuts").html(menuShortcuts);
+                ['items']['item'] = menuHandler.filterMenuItems(menuObj['items']['item']);
+                initMenuDefObj.resolve();
+            });
 
             //Add an event listener for clicking on menu items
             $('#menu').on('click', 'ul > li > a', function (e) {
--- webroot/test/ui/js/co.test.mock.data.js
+++ webroot/test/ui/js/co.test.mock.data.js
@@ -31,9 +31,9 @@ define(['underscore'], function (_) {
         webControllerMockData = {"webController": true},
         webStorageMockData = {"webStorage": true},
         serverManagerMockData = {"serverManager": true},
-        ctWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "webController": true }}),
-        smWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "serverManager": true }}),
-        sWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "webStorage": true }});
+        ctWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "webController": true }},{"featurePkgsInfo": { "webController": true },disabledFeatures:["config_alarms","mon_infra_mx"]}),
+        smWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "serverManager": true }},{"featurePkgsInfo": { "serverManager": true },disabledFeatures:["config_alarms","mon_infra_mx"]}),
+        sWebServerInfoMockData = _.extend({}, webServerInfoMockData, {"featurePkg": { "webStorage": true }},{"featurePkgsInfo": { "webStorage": true },disabledFeatures:["config_alarms","mon_infra_mx"]});
 
     return {
         ctWebServerInfoMockData: ctWebServerInfoMockData,
