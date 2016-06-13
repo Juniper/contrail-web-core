b30f8ab76972abdfb344b7e8302194f7d559aa9a
slimfire <yohazel2@gmail.com>
2016-06-13 11:05:06 -0700

    [FEATURE] add view renderer on json editor view

---
+++ webroot/js/views/JsonEditorView.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'contrail-view',
+    'knockback'
+], function (_, ContrailView, Knockback) {
+
+    var prefixId = smwc.SERVER_PREFIX_ID,
+        modalId = 'configure-' + prefixId,
+        editJSONTemplate = contrail.getTemplate4Id(cowc.TMPL_EDIT_JSON);
+
+    var JsonEditView = ContrailView.extend({
+
+        renderEditor: function (options) {
+            var editLayout = editJSONTemplate({prefixId: prefixId}),
+                disableId, modelAttr, self = this;
+            self.model.model().attributes = options['checkedRows'][0];
+            cowu.createModal({
+                modalId: modalId,
+                className: 'modal-980',
+                title: options['title'],
+                body: editLayout,
+                onSave: function () {
+                    var callbackObj = {
+                        init: function () {
+                            cowu.enableModalLoading(modalId);
+                        },
+                        success: function () {
+                            options['callback']();
+                            $("#" + modalId).modal('hide');
+                        },
+                        error: function (error) {
+                            cowu.disableModalLoading(modalId, function () {
+                                self.model.showErrorAttr(prefixId + cowc.FORM_SUFFIX_ID, error.responseText);
+                            });
+                        }
+                    };
+                    self.model.configure(options['checkedRows'], callbackObj, options['type']);
+                },
+                onCancel: function () {
+                    Knockback.release(self.model, document.getElementById(modalId));
+                    kbValidation.unbind(self);
+                    $("#" + modalId).modal('hide');
+                }
+            });
+
+            modelAttr = this.model.model().get('id');
+            disableId = (modelAttr == null || modelAttr == '') ? false : true;
+
+            self.renderView4Config($("#" + modalId).find("#" + prefixId + "-form"), this.model, JsonEditViewConfig, smwc.KEY_CONFIGURE_VALIDATION, null, null, function() {
+                Knockback.applyBindings(self.model, document.getElementById(modalId));
+            });
+        }
+    });
+
+    var JsonEditViewConfig = {
+        elementId: prefixId,
+        view: "JsonEditor",
+        viewConfig: []
+    };
+    return JsonEditView;
+});
