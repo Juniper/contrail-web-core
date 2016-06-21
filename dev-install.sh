a42cfd080780403597bbf03493e0ebc6af72511c
Dmitra Kia <k@dmitra.com>
2016-06-21 14:49:39 +0300

    fix css concatenation

--- dev-install.sh
+++ dev-install.sh
@@ -192,8 +192,8 @@ cp -r ./$THIRD_PARTY/dagre-v0.7.1/dist/dagre.js webroot/assets/joint/js/
 cp -r ./$THIRD_PARTY/joint-v0.9.3/dist/joint.layout.DirectedGraph.js webroot/assets/joint/js/
 cp -r ./$THIRD_PARTY/joint-v0.9.3/dist/joint.nojquery.min.css webroot/assets/joint/css/
 #new V0.9.6
-cp -r ./$THIRD_PARTY/joint-v0.9.6/js/joint.min.js webroot/assets/joint/js/
-cp -r ./$THIRD_PARTY/joint-v0.9.6/css/joint.min.css webroot/assets/joint/css/
+cp -r ./$THIRD_PARTY/joint-v0.9.6/dist/joint.min.js webroot/assets/joint/js/
+cp -r ./$THIRD_PARTY/joint-v0.9.6/dist/joint.min.css webroot/assets/joint/css/
 #End - Copy Joint from web-third-party
 
 #Start - Copy jquery-contextMenu from web-third-party
@@ -255,57 +255,6 @@ cp -af ./$THIRD_PARTY/gridstack-v0.2.5/dist/gridstack-extra.css webroot/assets/g
 
 #Start - Merging All CSS files
 rm -f webroot/css/contrail-all.css
-
-MAINCSS[1]=webroot/assets/bootstrap/css/bootstrap.min.css
-MAINCSS[2]=webroot/assets/bootstrap/css/bootstrap-responsive.min.css
-MAINCSS[3]=webroot/assets/jquery-ui/css/jquery-ui.min.css
-MAINCSS[4]=webroot/css/contrail.jquery.ui.css
-MAINCSS[5]=webroot/assets/font-awesome/css/font-awesome.min.css
-MAINCSS[6]=webroot/assets/nvd3-v1.8.1/css/nv.d3.min.css
-MAINCSS[7]=webroot/assets/select2/styles/select2.css
-MAINCSS[8]=webroot/assets/datetimepicker/styles/jquery.datetimepicker.css
-MAINCSS[9]=webroot/assets/slickgrid/styles/slick.grid.css
-MAINCSS[10]=webroot/assets/jquery/css/jquery.steps.css
-MAINCSS[11]=webroot/assets/joint/css/joint.nojquery.min.css
-MAINCSS[12]=webroot/assets/jquery-contextMenu/css/jquery.contextMenu.css
-MAINCSS[13]=webroot/assets/vis-v4.9.0/css/vis.min.css
-MAINCSS[14]=webroot/css/contrail.theme.layout.css
-MAINCSS[15]=webroot/css/contrail.theme.elements.css
-MAINCSS[16]=webroot/css/contrail.responsive.css
-MAINCSS[17]=webroot/css/contrail.grid.css
-MAINCSS[18]=webroot/css/contrail.form.elements.css
-MAINCSS[19]=webroot/css/contrail.components.css
-MAINCSS[20]=webroot/css/contrail.chart.css
-MAINCSS[21]=webroot/css/contrail.graph.css
-MAINCSS[22]=webroot/css/contrail.custom.css
-MAINCSS[23]=webroot/css/contrail.font.css
-MAINCSS[24]=webroot/css/login.css
-
-
-cat ${MAINCSS[1]} > webroot/css/contrail-all.css
-cat ${MAINCSS[2]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[3]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[4]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[5]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[6]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[7]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[8]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[9]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[10]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[11]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[12]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[13]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[14]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[15]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[16]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[17]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[18]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[19]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[20]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[21]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[22]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[23]} >> webroot/css/contrail-all.css
-cat ${MAINCSS[24]} >> webroot/css/contrail-all.css
 
 # compile sass
 ./node_modules/node-sass/bin/node-sass webroot/scss/index.scss > webroot/css/contrail-all.css
