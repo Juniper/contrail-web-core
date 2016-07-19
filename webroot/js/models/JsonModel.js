cbd5fe97dbaddeba32b5221b15b7ab9a11ada99b
czanpure <czanpure@juniper.net>
2016-07-19 15:39:16 -0700

    Partial-Bug: #1602902 Added JSON Editor to SM UI
    Change-Id: I8bddab6734ed25d9c0d976293d7cd567ede0ab5c

---
+++ webroot/js/models/JsonModel.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+ */
+
+define([
+    'underscore',
+    'backbone',
+    'knockout',
+    'contrail-model'
+], function (_, Backbone, Knockout, ContrailModel) {
+    var JsonModel = ContrailModel.extend({
+
+        defaultConfig: smwmc.getJSONModel(),
+
+        configure: function (checkedRows, callbackObj, type) {
+            var ajaxConfig = {};
+            var putData = {},
+                attributes = this.model().attributes;
+            putData[type] = [attributes];
+
+            ajaxConfig.type = "PUT";
+            ajaxConfig.data = JSON.stringify(putData);
+            ajaxConfig.url = smwu.getObjectUrl(type);
+
+            contrail.ajaxHandler(ajaxConfig, function () {
+                if (contrail.checkIfFunction(callbackObj.init)) {
+                    callbackObj.init();
+                }
+            }, function (response) {
+                callbackObj.success();
+                if (contrail.checkIfFunction(callbackObj.success)) {
+                    callbackObj.success();
+                }
+            }, function (error) {
+                if (contrail.checkIfFunction(callbackObj.error)) {
+                    callbackObj.error(error);
+                }
+            });
+        }
+    });
+
+    return JsonModel;
+});
