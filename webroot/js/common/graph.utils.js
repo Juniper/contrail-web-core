a24c22e0994d9f99e6c4b2d000daf259383e9d9d
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-09-09 18:21:46 -0700

    joint contrail and graph utils update
    - removed old file reference from require
    - moved graph utils to singleton, removed from global scope
    and will require from required file
    - contrail elements now part of core.bundle under production
    
    Change-Id: If1da5e3b3df7b7ad53d552696198d523c2ce526a

--- webroot/js/common/graph.utils.js
+++ webroot/js/common/graph.utils.js
@@ -3,31 +3,29 @@
  */
 
 define([
-    'underscore'
-], function (_) {
-
-    var GraphUtils = function () {
-        var self = this;
-
-        self.getImageName = function(node) {
-            var nodeStatus = node['status'],
-                serviceType = node['service_type'],
-                nodeType = node['node_type'],
+    //Uncomment following when core-constants is moved as singleton and not being used from global scope.
+    //"core-constants"
+], function () {
+    var graphUtils = {
+        getImageName : function(node) {
+            var nodeStatus = node.status,
+                serviceType = node.service_type,
+                nodeType = node.node_type,
                 imageName;
 
-            nodeType = (serviceType != null) ? (nodeType + '-' + serviceType) : nodeType;
+            nodeType = (serviceType !== null) ? (nodeType + "-" + serviceType) : nodeType;
             imageName = cowc.GRAPH_IMAGE_MAP[nodeType];
 
-            if (imageName == null) {
-                imageName = 'opencontrail-icon.png';
-            } else if (nodeStatus == 'Deleted') {
-                imageName += '-deleted.png';
+            if (imageName === null) {
+                imageName = "opencontrail-icon.png";
+            } else if (nodeStatus === "Deleted") {
+                imageName += "-deleted.png";
             } else {
-                imageName += '.png';
+                imageName += ".png";
             }
             return imageName;
-        };
+        }
     };
 
-    return GraphUtils;
+    return graphUtils;
 });
