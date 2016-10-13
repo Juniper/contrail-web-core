a09727ffe3706294b8b44e44ef27b9a61cbb3998
Naga Kiran <nagakiran@juniper.net>
2016-10-13 15:33:16 +0530

    Related-Bug: #1633013 Added render function for GridStackView which loops through widgetCfgList that's passed as part of viewConfig and adds widgets accordingly Added support for custom width/height Set gridStack cellHeight as 20px Makefile changes for importing gridstack
    Change-Id: I585337958478a3f77f2276c408ea6fed6550fccb

---
+++ webroot/js/views/GridStackView.js
@@ -0,0 +1,67 @@
+define([
+    'underscore',
+    'backbone',
+    'contrail-view',
+    'gridstack'
+], function (_, Backbone,ContrailView,gridstack) {
+    var GridStackView = ContrailView.extend({
+        initialize: function(options) {
+            var self = this;
+            self.widgets = [];
+            self.widgetCfgList = cowu.getValueByJsonPath(options,'attributes;viewConfig;widgetCfgList',{});
+            self.gridAttr = cowu.getValueByJsonPath(options,'attributes;viewConfig;gridAttr',{});
+            self.COLUMN_CNT = 2;
+            self.VIRTUAL_COLUMNS = 2;
+            //Default is 12 columns. To have 3 column layout, set defaultWidth as 4
+            if(self.gridAttr['defaultWidth'] != null)
+                self.COLUMN_CNT = 12/self.gridAttr['defaultWidth'];
+
+            self.$el.addClass('grid-stack grid-stack-12');
+            self.gridStack = $(self.$el).gridstack({
+                float:false,
+                handle:'header',
+                verticalMargin:8,
+                cellHeight: 20,
+                animate:false,
+                acceptWidgets:'label'
+            }).data('gridstack');
+            
+            //Trigger resize on widgets on resizestop
+        },
+        render: function() {
+            var self = this;
+            for(var i=0;i < self.widgetCfgList.length;i++) {
+                var currWidgetCfg = self.widgetCfgList[i];
+                self.add({
+                    modelCfg: currWidgetCfg['modelCfg'],
+                    viewCfg: currWidgetCfg['viewCfg'],
+                    itemAttr: currWidgetCfg['itemAttr']
+                });
+            }
+        },
+        add: function(cfg) {
+            var self = this;
+            var currElem = $($('#gridstack-widget-template').text()).attr('data-gs-height',2);
+            var itemAttr = ifNull(cfg['itemAttr'],{});
+            var defaultWidth = ifNull(self.gridAttr['defaultWidth'],1);
+            var defaultHeight = ifNull(self.gridAttr['defaultHeight'],1);
+            if(itemAttr['width'] != null) {
+                itemAttr['width'] = itemAttr['width']*defaultWidth;
+                $(currElem).attr('data-gs-width',itemAttr['width']);
+            }
+            if(itemAttr['height'] != null) {
+                itemAttr['height'] = itemAttr['height']*defaultHeight;
+                $(currElem).attr('data-gs-height',itemAttr['height']);
+            }
+            var widgetCnt = self.widgets.length;
+            self.gridStack.addWidget(currElem,widgetCnt/self.COLUMN_CNT,(widgetCnt%self.COLUMN_CNT)/**self.VIRTUAL_COLUMNS*/,
+                ifNull(itemAttr['width'],defaultWidth),ifNull(itemAttr['height'],defaultHeight),true);
+            self.widgets.push(currElem);
+            var modelCfg = cfg['modelCfg'];
+            if(cowu.getValueByJsonPath(cfg,'modelCfg;_type') != 'contrailListModel')
+                modelCfg = new ContrailListModel(modelCfg);
+            self.renderView4Config($(currElem).find('.item-content'), modelCfg, cfg['viewCfg']);
+        }
+    });
+    return GridStackView;
+});
