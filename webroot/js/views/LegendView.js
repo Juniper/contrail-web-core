74d09116616c2b3f07e2cf67820811d37c1ce705
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-10-24 13:40:07 -0700

    Fix legend view not getting updated on chart update
    - LegendView of a chart gets initialized and renderd on chart render.
    - on chart update we call the update of legend view with the updated
    view config using the updated data.
    - the lengend view update still calls the render. in future commit, the update
    logic needs be changed to not re-render. requires data binding and
    make the DOM update dynamic on data change.
    
    Change-Id: I41c536838cfa1003705840edd1386906a130998e
    Closes-Bug: #1636296
    UT: Executed 537 of 537 SUCCESS

--- webroot/js/views/LegendView.js
+++ webroot/js/views/LegendView.js
@@ -1,19 +1,37 @@
+/*
+ * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+ */
+
 define([
-    'underscore',
     'backbone'
-], function (_, Backbone) {
-    var legendView = Backbone.View.extend({
+], function (Backbone) {
+    var LegendView = Backbone.View.extend({
         initialize: function (args) {
-            var self = this,
-                legendConfig = getValueByJsonPath(args, 'legendConfig'),
-                selector = getValueByJsonPath(args, 'el'),
-                legendTemplate = contrail.getTemplate4Id(cowc.TMPL_CUSTOM_CHART_LEGEND);
-                if ($(selector).find('.custom-chart-legend') != null &&
-                        $(selector).find('.custom-chart-legend').length > 0) {
-                    $(selector).find('.custom-chart-legend').remove();
-                }
-                $(selector).prepend(legendTemplate(legendConfig));
+            var self = this;
+            self.selector = getValueByJsonPath(args, 'el');
+            self.legendTemplate = contrail.getTemplate4Id(cowc.TMPL_CUSTOM_CHART_LEGEND);
+            self.viewConfig = getValueByJsonPath(args, 'viewConfig');
+        },
+
+        setViewConfig: function (config) {
+            self.viewConfig = config;
+        },
+
+        render: function() {
+            var self = this;
+            if ($(self.selector).find('.custom-chart-legend') != null &&
+                $(self.selector).find('.custom-chart-legend').length > 0) {
+                $(self.selector).find('.custom-chart-legend').remove();
+            }
+            $(self.selector).prepend(self.legendTemplate(self.viewConfig));
+        },
+
+        update: function(config) {
+            var self = this;
+            if (config) self.setViewConfig(config);
+            //Todo Add one way binding with the config and update the DOM without having to re-render.
+            self.render();
         }
     });
-    return legendView;
+    return LegendView;
 });
