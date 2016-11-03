0fcf9b8cbf96d613a5e4e5126eade40c255caeb4
Sarin Kizhakkepurayil <sarink@juniper.net>
2016-11-03 13:39:14 -0700

    UDD enhancements and bug fixes
    - require 'udd-module' for core udd infra.
    - udd-module has all the related udd files bundled for prod-env
    - updated pageLoader similar to qe feature.
    - for the qe views, use the global layoutHandler. do not recreate.
    - form validation is specific to udd. moved to under udd/common and bundled with udd-module
    - udd default config - model is 'require'd directly so need to have 'core-basedir' alias.
    view files are required via viewConfig/renderView4Config logic which appends the alias based on app.
    - UT require path updates for qe-module and udd-module.
    - Test ran in both prod and dev env to make sure no failures on module addition.
    
    Change-Id: I560ec0f30ed5c28eb2bf9be7792f67a3f3161b01
    Closes-Bug: #1639045

--- webroot/reports/qe/ui/js/qe.module.js
+++ webroot/reports/qe/ui/js/qe.module.js
@@ -2,13 +2,24 @@
  * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
 
-/**
- * during build time, all view/model file of qe will be
- * concatenated to this file. build config is located in the core repo webroot/build/
- */
 define([
     "text!core-basedir/reports/qe/ui/templates/qe.tmpl",
-    "core-basedir/reports/qe/ui/js/QEPageLoader"
+    "core-basedir/reports/qe/ui/js/QEPageLoader",
+    //Additional included files in the module
+    "core-basedir/reports/qe/ui/js/common/qe.utils",
+    "core-basedir/reports/qe/ui/js/common/qe.parsers",
+    "core-basedir/reports/qe/ui/js/common/qe.grid.config",
+    "core-basedir/reports/qe/ui/js/common/qe.model.config",
+    "core-basedir/reports/qe/ui/js/views/QueryEngineView",
+    "core-basedir/reports/qe/ui/js/views/QueryQueueView",
+    "core-basedir/reports/qe/ui/js/views/QueryTextView",
+    "core-basedir/reports/qe/ui/js/views/ObjectLogsFormView",
+    "core-basedir/reports/qe/ui/js/views/SystemLogsFormView",
+    "core-basedir/reports/qe/ui/js/views/StatQueryFormView",
+    "core-basedir/reports/qe/ui/js/models/ContrailListModelGroup",
+    "core-basedir/reports/qe/ui/js/models/ObjectLogsFormModel",
+    "core-basedir/reports/qe/ui/js/models/StatQueryFormModel",
+    "core-basedir/reports/qe/ui/js/models/SystemLogsFormModel"
 ], function (QETemplates, QEPageLoader) {
     $("body").append(QETemplates);
     window.qePageLoader = new QEPageLoader();
