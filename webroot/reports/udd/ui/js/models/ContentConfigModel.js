9e458e4b73fdfb34b1b070217df6f74d01804321
Zheyang Song <zsong@juniper.net>
2016-11-15 18:38:59 -0800

    App is Broken due to Unexpected Event Firing Order
    * Add code logic to handle wrong order of change event handler
      execution, this is a special case only for "visualType" on
      data source config model
      	Affected files: WidgetModel.js
    * Adjusted logic of updating visible columns of GridView view
    	Affected files: GridConfigModel.js
    * Add logic of sanitizing initial model state passed in. This
      applies to all content/visual-meta config models
    	Affected files: all but WidgetModel.js and WidgetView.js
    
    Change-Id: I830e5b2b7f7470bb71bdc26b5c91dac123b31920
    Related-Bug: #1642116

--- webroot/reports/udd/ui/js/models/ContentConfigModel.js
+++ webroot/reports/udd/ui/js/models/ContentConfigModel.js
@@ -8,8 +8,12 @@ define([
 ], function(_, ContrailModel) {
 
     return ContrailModel.extend({
-        constructor: function() {
-            ContrailModel.prototype.constructor.apply(this, arguments);
+        constructor: function(modelConfig, modelRemoteDataConfig) {
+            /**
+             * Use contract defined by defaultConfig to sanitize modelConfig
+             */
+            modelConfig = this.normalizeModelConfig(modelConfig);
+            ContrailModel.prototype.constructor.call(this, modelConfig, modelRemoteDataConfig);
         },
         onDataModelChange: function() {},
 
@@ -23,6 +27,18 @@ define([
 
         getContentViewOptions: function() {
             return {};
+        },
+
+        normalizeModelConfig: function(modelConfig) {
+            if (this.defaultConfig) {
+                var picked = _.pick(modelConfig, function(propVal, propName) {
+                    return propName in this.defaultConfig;
+                }, this);
+
+                return picked;
+            } else {
+                return modelConfig;
+            }
         },
 
         timeSeries: function(fieldsString) {
