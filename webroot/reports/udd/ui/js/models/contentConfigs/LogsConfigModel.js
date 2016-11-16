a51164351ff302c6800daf81f45bd60c903aaa0d
Zheyang Song <zsong@juniper.net>
2016-11-15 18:38:59 -0800

    App is Broken due to Unexpected Event Firing Order
    * Add code logic to handle wrong order of change event handler
      execution, this is a special case only for "visualType" on
      data source config model
      	Affected files: WidgetModel.js
    * Adjusted logic of updating visible columns of GridView view
    	Affected files: GridConfigModel.js
    * Add logic of sanitizing initial model state passed in. This
      applies to all content/visual-meta config models
    	Affected files: all but WidgetModel.js and WidgetView.js
    
    Change-Id: I830e5b2b7f7470bb71bdc26b5c91dac123b31920
    Related-Bug: #1642116

--- webroot/reports/udd/ui/js/models/contentConfigs/LogsConfigModel.js
+++ webroot/reports/udd/ui/js/models/contentConfigs/LogsConfigModel.js
@@ -6,6 +6,10 @@ define([
     "core-basedir/reports/udd/ui/js/models/ContentConfigModel"
 ], function(ContentConfigModel) {
     return ContentConfigModel.extend({
+        constructor: function(modelConfig, modelRemoteDataConfig) {
+            ContentConfigModel.prototype.constructor.call(this, modelConfig, modelRemoteDataConfig);
+        },
+
         defaultConfig: {
             records: 5,
         },
