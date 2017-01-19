763920e05100437869f834eff66c5c81abac5d56
Zheyang Song <zsong@juniper.net>
2017-01-18 18:06:42 -0800

    Cross-Site Scripting (XSS) Contrail vulnerability
    + Add code to "HTML encode" the value of "engQueryStr" posted to "/api/admin/reports/query"
    
    Change-Id: If7ee8691e4fbeb8831f1b8bf0f62d767c76a0390
    Partial-Bug: #1657280

--- src/serverroot/utils/common.utils.js
+++ src/serverroot/utils/common.utils.js
@@ -2302,6 +2302,28 @@ function handleAuthToAuthorizeError(err, req, callback)
     }
 }
 
+function htmlEntityEncoding(srcString) {
+    if (!srcString) {
+        return srcString;
+    }
+
+    var encoder = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "\"": "&quot;",
+        "'": "&#x27;"
+    };
+
+    var retStr = srcString.replace(/./g, function(letter) {
+        var encoded = encoder[letter];
+
+        return encoded || letter;
+    });
+
+    return retStr;
+}
+
 exports.filterJsonKeysWithNullValues = filterJsonKeysWithNullValues;
 exports.createJSONBySandeshResponseArr = createJSONBySandeshResponseArr;
 exports.createJSONBySandeshResponse = createJSONBySandeshResponse;
@@ -2363,4 +2385,4 @@ exports.getValueByJsonPath = getValueByJsonPath;
 exports.getFeaturePkgs = getFeaturePkgs;
 exports.doDeepSort = doDeepSort;
 exports.handleAuthToAuthorizeError = handleAuthToAuthorizeError;
-
+exports.htmlEntityEncoding = htmlEntityEncoding;
