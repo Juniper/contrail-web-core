9c8a3e7122ffafb8b04afa6c82d86e41a37c71e5
Akhil Francis <akhilf@juniper.net>
2017-02-22 12:02:05 +0530

    Related-Bug:#1670560 Global Controller Dashboard UI Core changes
    Change-Id: Ib5da6e339c8a80fc0482e26cf2b1b6b8405b26a8

---
+++ webroot/js/views/globalControllerRegionView.js
@@ -0,0 +1,78 @@
+      /*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+define([
+    'underscore',
+    'contrail-view',
+    'monitor/infrastructure/globalcontroller/ui/js/models/globalControllerListModel'
+], function (_, ContrailView, GlobalControllerListModel) {
+    var GlobalControllerRegionView = ContrailView.extend({
+        render: function (viewConfig) {
+            var self = this,
+            globalControllerListModel=null;
+            globalControllerListModel = self.model;
+            self.model.onAllRequestsComplete.subscribe(function () {
+                var cfgDashbaordViewTemplate = contrail.getTemplate4Id("globalController-dashbaord-template");
+                var headInfo = globalControllerListModel.getItems()[0];
+                self.$el.html(cfgDashbaordViewTemplate({headInfo}));
+                var objectCntSection = self.$el.find(".container .objectCntSection"),
+                    chartSection = self.$el.find(".container .chartSection"),
+                    nodesCntSection = self.$el.find(".container .nodesCntContainer");
+                self.renderView4Config(objectCntSection,
+                        globalControllerListModel, getObjectCountView());
+                self.renderView4Config(chartSection,
+                        globalControllerListModel, getGlobalControllerChartsView(viewConfig));
+                self.renderView4Config(nodesCntSection,
+                        globalControllerListModel, getNodesCountView());
+            });
+        }
+    });
+    var getObjectCountView = function () {
+        return {
+            elementId: "objectviewCnt",
+            view: "SectionView",
+            viewConfig: {
+                rows: [{
+                    columns: [{
+                        elementId: "objectviewCnt_grid",
+                                view : "objectCntView",
+                        viewPathPrefix:"monitor/infrastructure/globalcontroller/ui/js/views/",
+                        app:cowc.APP_CONTRAIL_CONTROLLER
+                    }]}]
+                }
+        }
+    }
+    var getGlobalControllerChartsView = function (viewConfig) {
+        return {
+            elementId: "globalControllerChart",
+            view: "SectionView",
+            viewConfig: {
+                rows: [{
+                    columns: [{
+                        elementId: "globalControllerChart_grid",
+                        view : "globalControllerchartView",
+                        viewPathPrefix:"monitor/infrastructure/globalcontroller/ui/js/views/",
+                        app:cowc.APP_CONTRAIL_CONTROLLER
+                    }]}]
+                }
+        }
+    }
+    var getNodesCountView = function () {
+        return {
+            elementId: "nodesCounttView",
+            view: "SectionView",
+            viewConfig: {
+                rows: [{
+                    columns: [{
+                        elementId: "nodesCntView_grid",
+                        view: "nodesCntView",
+                        viewPathPrefix:"monitor/infrastructure/globalcontroller/ui/js/views/",
+                        app:cowc.APP_CONTRAIL_CONTROLLER,
+                        viewConfig: {
+                        }
+                    }]}]
+                }
+        }
+    }
+    return GlobalControllerRegionView;
+});
