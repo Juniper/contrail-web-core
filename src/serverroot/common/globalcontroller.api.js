6b3ae9b8dcd697018168d215ec0c7691867dd7ef
Biswajit Mandal <bmandal@juniper.net>
2017-03-11 08:36:44 -0800

    Related-Bug: #1558935 As we have dropdown of projects in gohan config page in stead of at login time, so we need to get the project token from keystone per project change. Project is retrieved from cookie and when any request comes from UI, then first checks the cache to see if we have token, else fetch from keystone and store.
    Change-Id: I6469ffaf831c458681a077326a76711a316823dd

--- src/serverroot/common/globalcontroller.api.js
+++ src/serverroot/common/globalcontroller.api.js
@@ -1,12 +1,14 @@
 /*
  * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
  */
+var rest        = require('../common/rest.api');
+var global      = require('./global');
+var config      = process.mainModule.exports.config;
+var authApi     = require('./auth.api');
+var logutils    = require('../utils/log.utils');
+var appErrors   = require('../errors/app.errors');
+var cgcConfig   = require('../../../webroot/config.json');
 var commonUtils = require('../utils/common.utils');
-var rest = require('../common/rest.api');
-var appErrors = require('../errors/app.errors');
-var global = require('./global');
-var config = process.mainModule.exports.config;
-var cgcConfig = require('../../../webroot/config.json');
 
 var cgcServerIP = ((config.cgc) && (config.cgc.server_ip)) ?
     config.cgc.server_ip : global.DFLT_SERVER_IP;
@@ -18,7 +20,6 @@ var cgcServer = rest.getAPIServer({apiName: global.label.CGC,
 
 function getCGCRestApiInst (req)
 {
-    var authApi = require('./auth.api');
     var oStack = require('../orchestration/plugins/openstack/openstack.api');
     var region =
         commonUtils.getValueByJsonPath(req, 'req;session;region', null, false);
@@ -41,6 +42,33 @@ function getCGCRestApiInst (req)
 
 function getCGCAllReq (req, res, next)
 {
+    var projectCookie = req.cookies.project;
+    var tokenToUse =
+        commonUtils.getValueByJsonPath(req, "session;tokenObjs;" + projectCookie
+                                       + ";token;id", null, false);
+    if (null != tokenToUse) {
+        logutils.logger.debug("We have token for project:" + projectCookie);
+        sendGohanRequest(req, res, tokenToUse);
+    } else {
+        /* Get the project token first */
+        var authObj = {req: req, project: projectCookie};
+        logutils.logger.debug("Trying to get new token for project:" +
+                              projectCookie);
+        authApi.getTokenObj(authObj, function(error, token, tokenObj) {
+            updateTokenByProject(req,
+                                 commonUtils.getValueByJsonPath(tokenObj,
+                                                                "access", null),
+                                 projectCookie);
+            tokenToUse =
+                commonUtils.getValueByJsonPath(tokenObj, "access;token;id",
+                                               null);
+            sendGohanRequest(req, res, tokenToUse);
+        });
+    }
+}
+
+function sendGohanRequest (req, res, token)
+{
     var handler = require('../web/routes/handler')
     if (!handler.isSessionAuthenticated(req)) {
         commonUtils.redirectToLogout(req, res);
@@ -58,7 +86,7 @@ function getCGCAllReq (req, res, next)
         return;
     }
     var tmpCGCRestObj = getCGCRestApiInst(req);
-    var headers = {'X-Auth-Token': req.session.last_token_used.id};
+    var headers = {'X-Auth-Token': token};
     if (null != tmpCGCRestObj['mapped']) {
         headers['protocol'] = tmpCGCRestObj.mapped.protocol;
     }
@@ -111,13 +139,16 @@ function getCGCAuthReq (req, res, next)
         next();
         return;
     }
-    var authApi = require('./auth.api');
     var reqUrl = splitArr[1];
  
     if (-1 != reqUrl.indexOf('/tokens')) {
         /* This is a token get request */
         var authObj = {req: req, project: req.cookies.project};
         authApi.getTokenObj(authObj, function(error, token, tokenObj) {
+            updateLastTokenUsed(req,
+                                commonUtils.getValueByJsonPath(tokenObj,
+                                                               "access;token",
+                                                               null));
             commonUtils.handleJSONResponse(error, res, tokenObj);
             return;
         });
@@ -125,8 +156,31 @@ function getCGCAuthReq (req, res, next)
     }
     authApi.getAuthRetryData(req.session.last_token_used, req, reqUrl,
                              function(error, data) {
+        updateLastTokenUsed(req, data);
         commonUtils.handleJSONResponse(error, res, data);
     });
+}
+
+function updateLastTokenUsed (req, token)
+{
+    if (null != token) {
+        req.session.last_token_used = token;
+    }
+}
+
+function updateTokenByProject (req, tokenObj, project)
+{
+    if (null == tokenObj) {
+        return;
+    }
+    var tokenObjs = commonUtils.getValueByJsonPath(req, "session;tokenObjs",
+                                                   null, false);
+    if (null == tokenObjs) {
+        req.session.tokenObjs = {};
+    }
+    req.session.tokenObjs[project] = tokenObj;
+    updateLastTokenUsed(req, commonUtils.getValueByJsonPath(tokenObj, "token",
+                                                            null));
 }
 
 exports.getCGCAllReq = getCGCAllReq;
