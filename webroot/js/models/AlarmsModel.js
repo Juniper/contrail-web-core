6b66a58fe69353d14a4b00aa91fd87ed01c4f0a3
rajivks <rajivks@juniper.net>
2017-04-12 06:15:03 -0700

    Closes-Bug:#1682109- All Regions - Consolidate alarms from all regions in the popup
    Change-Id: I49fdc64cd8b5d23992ab1d215036eb3c5938646d

--- webroot/js/models/AlarmsModel.js
+++ webroot/js/models/AlarmsModel.js
@@ -9,33 +9,68 @@ define([
 
     var alarmsModel = ContrailModel.extend({
         ackAlarms: function (checkedRows, callbackObj) {
-            var ajaxConfig = {};
-            var alarmsList = [];
+            var ajaxConfig = {}, alarmsList = [],
+            getAjaxs = [], regionBasedRecord = {};
+            var region = contrail.getCookie('region');
+            if(region != 'All Regions'){
+                $.each(checkedRows, function (checkedRowsKey, checkedRowsValue) {
+                    alarmsList.push({"table":checkedRowsValue.table,
+                        "name":checkedRowsValue.name,
+                        "type":checkedRowsValue.type,
+                        "token":checkedRowsValue.token});
+                });
 
-            $.each(checkedRows, function (checkedRowsKey, checkedRowsValue) {
-                alarmsList.push({"table":checkedRowsValue.table,
-                    "name":checkedRowsValue.name,
-                    "type":checkedRowsValue.type,
-                    "token":checkedRowsValue.token});
-            });
-
-            ajaxConfig.type = "POST";
-            ajaxConfig.data = JSON.stringify(alarmsList);
-
-            ajaxConfig.url = '/api/tenant/monitoring/ackalarms';
-            contrail.ajaxHandler(ajaxConfig, function () {
-                if (contrail.checkIfFunction(callbackObj.init)) {
-                    callbackObj.init();
+                ajaxConfig.type = "POST";
+                ajaxConfig.data = JSON.stringify(alarmsList);
+                ajaxConfig.url = '/api/tenant/monitoring/ackalarms';
+                contrail.ajaxHandler(ajaxConfig, function () {
+                    if (contrail.checkIfFunction(callbackObj.init)) {
+                        callbackObj.init();
+                    }
+                }, function (response) {
+                    if (contrail.checkIfFunction(callbackObj.success)) {
+                        callbackObj.success();
+                    }
+                }, function (error) {
+                    if (contrail.checkIfFunction(callbackObj.error)) {
+                        callbackObj.error(error);
+                    }
+                });
+            }else{
+                for(var i = 0; i < checkedRows.length; i++){
+                    var obj = {"table":checkedRows[i].table, "name":checkedRows[i].name,
+                               "type":checkedRows[i].type, "token":checkedRows[i].token,"reqRegion": checkedRows[i].regionName};
+                    if(Object.keys(regionBasedRecord).length === 0){
+                        regionBasedRecord[checkedRows[i].regionName] = [];
+                        regionBasedRecord[checkedRows[i].regionName].push(obj);
+                    }else{
+                        var keys = Object.keys(regionBasedRecord);
+                        if(keys.indexOf(checkedRows[i].regionName) !== -1){
+                            regionBasedRecord[checkedRows[i].regionName].push(obj);
+                        }else{
+                            regionBasedRecord[checkedRows[i].regionName] = [];
+                            regionBasedRecord[checkedRows[i].regionName].push(obj);
+                        }
+                    }
                 }
-            }, function (response) {
-                if (contrail.checkIfFunction(callbackObj.success)) {
-                    callbackObj.success();
+                var region = Object.keys(regionBasedRecord);
+                for(var j = 0; j < region.length; j++){
+                    var data = regionBasedRecord[region[j]];
+                    getAjaxs[j] = $.ajax({
+                        url: '/api/tenant/monitoring/ackalarms?reqRegion='+ region[j],
+                        type:'POST',
+                        contentType:'application/json; charset=utf-8',
+                        data: JSON.stringify(data),
+                        dataType:'json'
+                        
+                    });
                 }
-            }, function (error) {
-                if (contrail.checkIfFunction(callbackObj.error)) {
-                    callbackObj.error(error);
-                }
-            });
+                $.when.apply($, getAjaxs).then( function () {
+                    if (contrail.checkIfFunction(callbackObj.success)) {
+                        callbackObj.success();
+                    }
+                })
+            }
         },
     });
 
