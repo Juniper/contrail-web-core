6b7fd5696c04c20befc2905d5a69098185e3fd19
Biswajit Mandal <bmandal@juniper.net>
2017-05-12 14:26:15 -0700

    Closes-Bug: #1689382 Changing the SSL cipher suite based on recommendations: https://wiki.mozilla.org/Security/Server_Side_TLS With this old and intermediate level(Good number of browser support) tests passed. Modern level may not get fixed, as that may impact some older browsers from support.
    SSL Test:
    =========
    root@bmandal-ci-ubuntu16:~/cipherscan# ./analyze.py -t https://a7s12.englab.juniper.net:8143
    a7s12.englab.juniper.net:8143 has bad ssl/tls
    
    Things that are bad:
    * don't use an untrusted or self-signed certificate
    
    Changes needed to match the old level:
    * enable SSLv3
    * use a certificate with sha1WithRSAEncryption signature
    * consider enabling OCSP Stapling
    
    Changes needed to match the intermediate level:
    * consider enabling OCSP Stapling
    
    Changes needed to match the modern level:
    * remove cipher AES128-GCM-SHA256
    * remove cipher AES256-GCM-SHA384
    * remove cipher AES128-SHA256
    * remove cipher AES256-SHA256
    * remove cipher AES128-SHA
    * remove cipher AES256-SHA
    * remove cipher DES-CBC3-SHA
    * disable TLSv1.1
    * disable TLSv1
    * consider enabling OCSP Stapling
    root@bmandal-ci-ubuntu16:~/cipherscan#
    
    Change-Id: Id1b02dcc1fae3fb1b87df9c327fd0a8766eb9252

--- config/default.config.global.js
+++ config/default.config.global.js
@@ -466,7 +466,17 @@ config.proxy.config_node_ports = [
  *****************************************************************************/
 config.server_options = {};
 config.server_options.ciphers =
-    'ECDHE-RSA-AES256-SHA384:AES256-SHA256:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM';
+    'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:' +
+    'ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:' +
+    'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:' +
+    'DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:' +
+    'ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:' +
+    'ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:' +
+    'ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:' +
+    'DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:' +
+    'ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:' +
+    'AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:' +
+    'AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
 config.server_options.key_file = '/usr/src/contrail/contrail-web-core/keys/cs-key.pem';
 config.server_options.cert_file = '/usr/src/contrail/contrail-web-core/keys/cs-cert.pem';
 
