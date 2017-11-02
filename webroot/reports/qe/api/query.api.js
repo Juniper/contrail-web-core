d54a54990c85c7ce79932d19ef40a8e19de43781
Manoj Naik <mnaik@juniper.net>
2017-11-02 14:45:26 +0530

    Implemented the Query page for Session Series and Session Record.
    Add new pages to query the Session Series and Record tables.
    
    Change-Id: Ib70e565a967688cf3c9f15b4b318dea1d9ed294c
    Related-Bug: #1729535
    (cherry picked from commit 7eb3aef306443d26731239a948f4bbd6030271ed)

--- webroot/reports/qe/api/query.api.js
+++ webroot/reports/qe/api/query.api.js
@@ -274,6 +274,8 @@ function getQueryOptions(queryReqObj) {
         queryOptions.queryQueue = "fqq";
     } else if (tableType === "STAT") {
         queryOptions.queryQueue = "sqq";
+    } else if (tableType === "SESSION") {
+        queryOptions.queryQueue = "seqq";
     }
 
     return queryOptions;
@@ -933,7 +935,8 @@ function getQueryJSON4Table(queryReqObj) {
         where = formModelAttrs.where,
         filters = formModelAttrs.filters,
         autoSort = queryReqObj.autoSort,
-        direction = formModelAttrs.direction;
+        direction = formModelAttrs.direction,
+        sessionType = formModelAttrs.session_type;
 
     autoSort = (!_.isNil(autoSort) && (autoSort === "true" || autoSort)) ? true : false;
 
@@ -962,15 +965,27 @@ function getQueryJSON4Table(queryReqObj) {
         queryJSON = _.extend({}, queryJSON, { "select_fields": ["flow_class_id", "direction_ing"] });
 
         if (autoSort) {
-            if (select.indexOf("T=") !== -1) {
-                queryJSON.select_fields.push("T");
+            if(select.indexOf("T=") === -1) {
+                queryJSON.sort_fields = ["T"];
+                queryJSON.sort = 2;
             }
-            queryJSON.sort_fields = ["T"];
-            queryJSON.sort = 2;
         }
 
     } else if (tableName === "FlowRecordTable") {
         queryJSON = _.extend({}, queryJSON, { "select_fields": ["direction_ing"] });
+
+    } else if (tableName === "SessionSeriesTable") {
+        autoSort = (select.indexOf("T=") === -1 && select.indexOf("T") === -1) ? false : autoSort;
+        queryJSON = _.extend({}, queryJSON, { "session_type" : sessionType});
+        if (autoSort) {
+            if(select.indexOf("T=") === -1) {
+                queryJSON.sort_fields = ["T"];
+                queryJSON.sort = 2;
+            }
+        }
+
+    } else if (tableName === "SessionRecordTable") {
+        queryJSON = _.extend({}, queryJSON, { "session_type" : sessionType});
 
     } else if (tableType === "OBJECT") {
         autoSort = (select.indexOf("MessageTS") === -1) ? false : autoSort;
