bca6c25b62e71f9695aa880bbea1f3889103c9de
Hariharan Suryakumar <harisk@juniper.net>
2018-01-23 10:06:05 +0530

    Back To Top and Scroll related issues
    1) Back to top is not shown
    2) Header should hide on scroll
    3) Scroll event causing jerks in the config pages.
    4) Menu separation line is not extended till the end.
    
    Change-Id: Id6b9bab35fed73f02d98d4e298272ec9f6865611
    Closes-Bug: #1744851

--- webroot/js/contrail-load.js
+++ webroot/js/contrail-load.js
@@ -3,7 +3,6 @@
  */
 
 $(document).ready(function () {
-
     //Listener to expand/collapse widget based on toggleButton in widget header
     $("#content-container").on('click', 'div.widget-box div.widget-header div.widget-toolbar a[data-action="collapse"]', function () {
         $(this).find('i').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
@@ -53,6 +52,7 @@ $(document).ready(function () {
 
                 thisParent.find('.group-detail-advanced-action-item').removeClass('selected');
                 $(this).addClass('selected');
+;
 
                 if (contrail.checkIfExist($(this).parents('.slick-row-detail').data('cgrid'))) {
                     $(this).parents('.contrail-grid').data('contrailGrid').adjustDetailRowHeight($(this).parents('.slick-row-detail').data('cgrid'));
@@ -86,7 +86,12 @@ $(document).ready(function () {
             }
         });
 
+<<<<<<< HEAD
     $(window).on('scroll', function () {
+=======
+
+    /*$(window).on('scroll', function () {
+>>>>>>> 719a1e9... Back To Top and Scroll related issues
         var scrollHeight = $(document).height() - $(window).height(),
             previousScroll = 0,
             currentScroll = $(this).scrollTop();
@@ -112,14 +117,118 @@ $(document).ready(function () {
         if (currentScroll < scrollHeight) {
             previousScroll = $(window).scrollTop();
         }
+<<<<<<< HEAD
+    });
+=======
+    });*/
+ // ------------------  Scroll start plugin start
+    (function(){
+            var special = jQuery.event.special,
+            uid1 = 'D' + (+new Date()),
+            uid2 = 'D' + (+new Date() + 1);
+
+        special.scrollstart = {
+            setup: function() {
+
+                var timer,
+                    handler =  function(evt) {
+
+                        var _self = this,
+                            _args = arguments;
+
+                        if (timer) {
+                            clearTimeout(timer);
+                        } else {
+                            evt.type = 'scrollstart';
+                           // jQuery.event.handle.apply(_self, _args);
+                            jQuery.event.dispatch.apply(_self, _args);
+                        }
+
+                        timer = setTimeout( function(){
+                            timer = null;
+                        }, special.scrollstop.latency);
+
+                    };
+
+                jQuery(this).bind('scroll', handler).data(uid1, handler);
+
+            },
+            teardown: function(){
+                jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
+            }
+        };
+
+        special.scrollstop = {
+            latency: 50,
+            setup: function() {
+
+                var timer,
+                        handler = function(evt) {
+>>>>>>> 719a1e9... Back To Top and Scroll related issues
+
+                        var _self = this,
+                            _args = arguments;
+
+                        if (timer) {
+                            clearTimeout(timer);
+                        }
+
+                        timer = setTimeout( function(){
+
+                            timer = null;
+                            evt.type = 'scrollstop';
+                            jQuery.event.dispatch.apply(_self, _args);
+
+                        }, special.scrollstop.latency);
+
+                    };
+
+                jQuery(this).bind('scroll', handler).data(uid2, handler);
+
+            },
+            teardown: function() {
+                jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
+            }
+        };
+
+    })();
+    // -----------------  plugin end
+
+    var before = 0;
+
+
+    $(window).bind('scrollstart', function() {
+        before = $(window).scrollTop();
     });
 
-    $(document).on('click', '#back-to-top', function (event) {
-        event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 500);
-        return false;
-    });
+    $(window).bind('scrollstop', function() {
+        console.log('before: ' + before + "\nafter: " + $(window).scrollTop())
+         var scrollHeight = $(document).height() - $(window).height(),
+            previousScroll = 0,
+            currentScroll = $(this).scrollTop();
 
+        if (currentScroll < 45 || previousScroll - currentScroll > 40) {
+            $("#pageHeader").show();
+            $('#page-content').removeClass('scrolled');
+            $('#sidebar').removeClass('scrolled');
+            $('#breadcrumbs').removeClass('scrolled');
+            $('#back-to-top').fadeOut();
+            $('#page-content').css('z-index',0);
+        }
+        else {
+            $("#pageHeader").hide();
+            $('#page-content').addClass('scrolled');
+            $('#sidebar').addClass('scrolled');
+            $('#breadcrumbs').addClass('scrolled');
+            $('#back-to-top').fadeIn();
+            $('#page-content').css('z-index',-1);
+        }
+        if (currentScroll < scrollHeight) {
+            previousScroll = $(window).scrollTop();
+        }
+        before = $(window).scrollTop();
+
+    });
 
     // layoutHandler.load();
 
@@ -477,3 +586,32 @@ $(document).on('click', '.pre-format-JSON2HTML .collapser', function(){
     }
 	
 })(jQuery);
+
+/*document.arrive('[data-spy="affix"]', function() {
+    console.info("affix element added", $(this));
+    activateAffixPlugin($(this));
+});*/
+
+function activateAffixPlugin(elem) {
+    var data = $(elem).data();
+    data.offset = data.offset || {}
+
+    if (data.offsetBottom != null) data.offset.bottom = data.offsetBottom;
+    if (data.offsetTop    != null) data.offset.top    = data.offsetTop;
+    var $spy = $(elem).affix(data);
+}
+
+
+$(document).ready(function() {
+
+    if(typeof($().affix) == 'function') {
+        $('[data-spy="affix"]').each(function() { 
+            var data = $(this).data();
+            data.offset = data.offset || {}
+
+            if (data.offsetBottom != null) data.offset.bottom = data.offsetBottom;
+            if (data.offsetTop    != null) data.offset.top    = data.offsetTop;
+            var $spy = $(this).affix(data);
+        });
+    }
+});
