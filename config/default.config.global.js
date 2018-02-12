b7f0df3b45bc6a03886bb281d9ed25cec12eaf64
Siva Kumar Bavanasi <kbsiva@juniper.net>
2018-02-12 10:35:32 +0530

    Added cassandra's username&password entries to config
    Change-Id: I8636ef00c760402db110ec01eb49ba6f02a2c753
    Closes-bug: #1746719

--- config/default.config.global.js
+++ config/default.config.global.js
@@ -205,6 +205,8 @@ config.cassandra = {};
 config.cassandra.server_ips = ['127.0.0.1'];
 config.cassandra.server_port = '9042';
 config.cassandra.enable_edit = false;
+config.cassandra.username = '';
+config.cassandra.password = '';
 
 /* KUE Job Scheduler */
 config.kue = {};
