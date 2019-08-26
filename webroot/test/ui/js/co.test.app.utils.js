f561509bdbaf584118e1824127d1bfb0a51372aa
ZheyangSong <jerome_soung@hotmail.com>
2019-08-26 13:45:42 -0700

    Web Vulnerabilities Fix
    + Upgrade lodash to 4.17.12 --- app-wide
    + Use moment.js 2.14.1 --- backend
    + Upgrade expressJS to 4.8.8 --- backend
      + this change introduces new depencences:
        + body-parser@1.19.0
        + compression@1.7.4
        + method-override@3.0.0
        + cookie-parser@1.4.4
        + express-session@1.16.2
        + csurf@1.10.0
    + Upgrade connect-redis to 2.5.1 --- required by expressJS@4
    
    partial-jira-bug: CEM-8320
    
    Change-Id: Ie141b3e92fc4226ed82610f2e52d35a9711df82a

--- webroot/test/ui/js/co.test.app.utils.js
+++ webroot/test/ui/js/co.test.app.utils.js
@@ -49,7 +49,7 @@ function getCoreAppPaths(coreBaseDir, coreBuildDir, env) {
         'underscore'            : coreWebDir + '/assets/underscore/underscore-min',
         'slickgrid-utils'       : coreWebDir + "/js/slickgrid-utils",
         'jquery'                : coreWebDir + '/assets/jquery/js/jquery.min',
-        'lodashv4'                    : coreWebDir + '/assets/lodash-v4.17.2/js/lodash',
+        'lodashv4'                    : coreWebDir + '/assets/lodash-v4.17.12/js/lodash.min',
         'd3v4'                        : coreWebDir + '/assets/d3-v4.4.3/js/d3.min',
         'contrail-load'         : coreWebDir + '/js/contrail-load',
         'vis'                   : coreWebDir + '/assets/vis-v4.9.0/js/vis.min',
