55091fea8b1ac89c286142c45c60cbbe869a2e1a
Andrey Pavlov <andrey-mp@yandex.ru>
2020-01-15 23:57:59 +0300

    [Migration] Remove content from Juniper
    Automated change
    
    Depends-On: I30ef97ca4a019167d786fbc4047352fa0731c49f
    Depends-On: I05591392b3c0af32430be3d67c4282d3207b976b
    Depends-On: I7c2ea115f4493f6015cb2d2669064ec5eacc4f4d
    Change-Id: Iad8973bf496d71864b03fcc2e7cba8ccdd737ab8

--- webroot/build/core.css.build.js
+++
@@ -1,5 +0,0 @@
-({
-    cssIn: './../common/ui/css/contrail.unified.css',
-    out: './../common/ui/css/contrail.unified.min.css',
-    optimizeCss: 'default'
-})
--- webroot/js/models/SparklineModel.js
+++
@@ -1,8 +0,0 @@
-define([
-    'underscore'
-], function (_) {
-    var SparklineModel = function(chartOptions) {
-        
-    }
-    return SparklineModel;
-});
